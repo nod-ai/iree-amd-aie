23898986950bf3425986cce2734fd1a3242dbb71
nirvedhmeshram <96096277+nirvedhmeshram@users.noreply.github.com>
2023-11-03 10:11:21 -0500

    Add initial XRT runtime cmake integration (#5)
    This just has a small test to verify that we can access the XRT API

---
+++ runtime/plugins/XRT/runtime/iree-test-xrt.cc
@@ -0,0 +1,17 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#include <iostream>
+
+#include "experimental/xrt_xclbin.h"
+#include "xclbin.h"
+#include "xrt.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_kernel.h"
+int main() {
+  auto device = xrtDeviceOpen(0);
+  std::cout << "device at: " << &device << std::endl;
+  return 0;
+}
