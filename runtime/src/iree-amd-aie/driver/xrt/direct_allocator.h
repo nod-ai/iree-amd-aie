d0dae5593a9253be59f6f539959de2c56d95d86b
Nirvedh Meshram <nmeshram@amd.com>
2023-11-14 02:39:01 +0000

    add allocator and buffer implementation

---
+++ runtime/src/iree-amd-aie/driver/xrt/direct_allocator.h
@@ -0,0 +1,27 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_AMD_AIE_DRIVER_XRT_DIRECT_ALLOCATOR_H_
+#define IREE_AMD_AIE_DRIVER_XRT_DIRECT_ALLOCATOR_H_
+
+#include "iree/base/api.h"
+#include "iree/hal/api.h"
+#include "xrt/xrt_bo.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+// Creates an XRT memory allocator.
+iree_status_t iree_hal_xrt_allocator_create(
+    xrt::device device, iree_allocator_t host_allocator,
+    iree_hal_allocator_t** out_allocator);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // IREE_AMD_AIE_DRIVER_XRT_DIRECT_ALLOCATOR_H_
