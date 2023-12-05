06fcf72f92a1c22b252dc3ab0bb1a2e89b1fc9bc
Nirvedh Meshram <nmeshram@amd.com>
2023-12-05 04:18:13 +0000

    [XRT] Add command buffer
    With this commit we can start testing to see if we can launch kernels.
    Some changes are made to the pipeline layout to better suit the needs
    of XRT API.

--- runtime/src/iree-amd-aie/driver/xrt/pipeline_layout.h
+++ runtime/src/iree-amd-aie/driver/xrt/pipeline_layout.h
@@ -14,6 +14,35 @@
 extern "C" {
 #endif  // __cplusplus
 
+// The max number of bindings per descriptor set allowed in the XRT HAL
+// implementation.
+#define IREE_HAL_XRT_MAX_DESCRIPTOR_SET_BINDING_COUNT 16
+
+// The max number of descriptor sets allowed in the XRT HAL implementation.
+//
+// This depends on the general descriptor set planning in IREE and should adjust
+// with it.
+#define IREE_HAL_XRT_MAX_DESCRIPTOR_SET_COUNT 4
+
+// Note that IREE HAL uses a descriptor binding model for expressing resources
+// to the kernels--each descriptor specifies the resource information, together
+// with a (set, binding) number indicating which "slots" it's bound to.
+//
+// In XRT, however, we don't have a direct correspondance of such mechanism.
+// Resources are expressed as kernel arguments. Therefore to implement IREE
+// HAL descriptor set and pipepline layout in XRT, we order and flatten all
+// sets and bindings and map to them to a linear array of kernel arguments.
+//
+// For example, given a pipeline layout with two sets and two bindings each:
+//   (set #, binding #) | kernel argument #
+//   :----------------: | :---------------:
+//   (0, 0)             | 2
+//   (0, 4)             | 3
+//   (2, 1)             | 4
+//   (2, 3)             | 5
+// Note that currently first two arguments are reserved for LX6 asm intruction
+// stream related arguments
+
 //===----------------------------------------------------------------------===//
 // iree_hal_xrt_descriptor_set_layout_t
 //===----------------------------------------------------------------------===//
@@ -29,12 +58,9 @@ iree_status_t iree_hal_xrt_descriptor_set_layout_create(
     iree_allocator_t host_allocator,
     iree_hal_descriptor_set_layout_t** out_descriptor_set_layout);
 
-// Returns the information about a given |binding| in
-// |base_descriptor_set_layout|.
-iree_hal_descriptor_set_layout_binding_t*
-iree_hal_xrt_descriptor_set_layout_binding(
-    iree_hal_descriptor_set_layout_t* base_descriptor_set_layout,
-    uint32_t binding);
+// Returns the binding count for the given descriptor set layout.
+iree_host_size_t iree_hal_xrt_descriptor_set_layout_binding_count(
+    const iree_hal_descriptor_set_layout_t* base_descriptor_set_layout);
 
 //===----------------------------------------------------------------------===//
 // iree_hal_xrt_pipeline_layout_t
@@ -56,6 +82,14 @@ iree_status_t iree_hal_xrt_pipeline_layout_create(
 iree_hal_descriptor_set_layout_t*
 iree_hal_xrt_pipeline_layout_descriptor_set_layout(
     iree_hal_pipeline_layout_t* base_pipeline_layout, uint32_t set);
+
+// Returns the total number of sets in the given |pipeline_layout|.
+iree_host_size_t iree_hal_xrt_pipeline_layout_descriptor_set_count(
+    const iree_hal_pipeline_layout_t* pipeline_layout);
+
+// Returns the base kernel argument index for the given set.
+iree_host_size_t iree_hal_xrt_pipeline_layout_base_binding_index(
+    const iree_hal_pipeline_layout_t* pipeline_layout, uint32_t set);
 
 #ifdef __cplusplus
 }  // extern "C"
