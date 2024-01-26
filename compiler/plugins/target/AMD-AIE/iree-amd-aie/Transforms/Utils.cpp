b779b048b77fbe635e1bbf803ed3272aa87462ed
Vivian <vivian.zhang@amd.com>
2024-01-26 14:53:58 -0800

    Add pass to fuse fill op into forall loop (#107)

---
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/Utils.cpp
@@ -0,0 +1,122 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "iree-amd-aie/Transforms/Utils.h"
+
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+
+namespace mlir::iree_compiler::AMDAIE {
+
+//===----------------------------------------------------------------------------------===//
+// Methods copied over from core to implement tile and fuse with scf.forall.
+// TODO(MaheshRavishankar) Replace them with core methods when upstream
+// can handle this natively
+//===----------------------------------------------------------------------------------===//
+/// Clones the operation and updates the destination if the operation
+/// implements the `DestinationStyleOpInterface`.
+static Operation *cloneOpAndUpdateDestinationArgs(RewriterBase &rewriter,
+                                                  Operation *op,
+                                                  ValueRange newDestArgs) {
+  Operation *clonedOp = rewriter.clone(*op);
+  if (newDestArgs.empty()) return clonedOp;
+  if (auto destinationStyleOp = dyn_cast<DestinationStyleOpInterface>(clonedOp))
+    destinationStyleOp.getDpsInitsMutable().assign(newDestArgs);
+  return clonedOp;
+}
+
+/// Return the untiled producer whose slice is used in a tiled consumer. If
+/// there was no loop traversal needed, the second value of the returned tuple
+/// is empty. In case the source op is a linalg.copy it returns both values as
+/// empty.
+static std::tuple<OpResult, std::optional<OpOperand *>>
+getUntiledProducerFromSliceSource(OpOperand *source, scf::ForallOp loop) {
+  std::optional<OpOperand *> destinationSharedOuts;
+  if (auto sharedOuts = dyn_cast<BlockArgument>(source->get())) {
+    if (sharedOuts.getOwner()->getParentOp() == loop) {
+      source = loop.getTiedOpOperand(sharedOuts);
+      destinationSharedOuts = source;
+    }
+  } else if (isa<linalg::CopyOp>(source->get().getDefiningOp()))
+    return {nullptr, nullptr};
+  return {dyn_cast<OpResult>(source->get()), destinationSharedOuts};
+}
+
+/// The following is a copy of tileAndFuseProducerOfSlice to make it work for
+/// scf.forall.
+/// Implementation of fusing producer of a single slice by computing the
+/// slice of the producer in-place.
+std::optional<scf::SCFFuseProducerOfSliceResult> tileAndFuseProducerOfSlice(
+    RewriterBase &rewriter, tensor::ExtractSliceOp candidateSliceOp,
+    scf::ForallOp &loop) {
+  // 1. Get the producer of the source.
+  auto [fusableProducer, destinationInitArg] =
+      getUntiledProducerFromSliceSource(&candidateSliceOp.getSourceMutable(),
+                                        loop);
+  if (!fusableProducer) return std::nullopt;
+  unsigned resultNumber = fusableProducer.getResultNumber();
+
+  OpBuilder::InsertionGuard g(rewriter);
+  rewriter.setInsertionPoint(candidateSliceOp);
+
+  // 2. Clone the fused producer
+  // 2a. Compute the destination operands to use for the cloned operation.
+  SmallVector<Value> origDestinationTensors, clonedOpDestinationTensors;
+  Operation *fusableProducerOp = fusableProducer.getOwner();
+  if (isa<DestinationStyleOpInterface>(fusableProducerOp) &&
+      failed(tensor::getOrCreateDestinations(
+          rewriter, fusableProducerOp->getLoc(), fusableProducerOp,
+          origDestinationTensors)))
+    return std::nullopt;
+
+  clonedOpDestinationTensors = origDestinationTensors;
+  if (destinationInitArg &&
+      isa<DestinationStyleOpInterface>(fusableProducerOp)) {
+    // 2b. If the producer is also destination style, then to maintain the
+    // destination passing style, update the destination of the producer to be
+    // the source of the slice.
+    clonedOpDestinationTensors[resultNumber] = candidateSliceOp.getSource();
+  }
+  // 2c. Clone the fused producer.
+  Operation *clonedProducerOp = cloneOpAndUpdateDestinationArgs(
+      rewriter, fusableProducerOp, clonedOpDestinationTensors);
+  // 2d. Update the source of the candidateSlice to be the cloned producer.
+  //     Easier to just clone the slice with different source since replacements
+  //     and DCE of cloned ops becomes easier
+  SmallVector<Value> candidateSliceOpOperands =
+      llvm::to_vector(candidateSliceOp->getOperands());
+  candidateSliceOpOperands[0] = clonedProducerOp->getResult(resultNumber);
+  tensor::ExtractSliceOp clonedCandidateSliceOp =
+      mlir::clone(rewriter, candidateSliceOp,
+                  candidateSliceOp->getResultTypes(), candidateSliceOpOperands);
+
+  // 3. Generate the tiled implementation of the producer of the source
+  FailureOr<TilingResult> tileAndFuseResult =
+      tensor::replaceExtractSliceWithTiledProducer(
+          rewriter, clonedCandidateSliceOp,
+          clonedProducerOp->getResult(resultNumber));
+  if (failed(tileAndFuseResult)) return std::nullopt;
+  // Note: Do not delete the candidateSliceOp, since its passed in from the
+  // caller.
+  rewriter.replaceAllUsesWith(candidateSliceOp,
+                              tileAndFuseResult->tiledValues[0]);
+  rewriter.eraseOp(clonedCandidateSliceOp);
+  rewriter.eraseOp(clonedProducerOp);
+
+  if (destinationInitArg &&
+      isa<DestinationStyleOpInterface>(fusableProducerOp) && loop) {
+    loop->getOpOperands()[destinationInitArg.value()->getOperandNumber()].set(
+        origDestinationTensors[resultNumber]);
+  }
+  return scf::SCFFuseProducerOfSliceResult{fusableProducer,
+                                           tileAndFuseResult->tiledValues[0],
+                                           tileAndFuseResult->tiledOps};
+}
+
+//===----------------------------------------------------------------------------------===//
+// End methods copied over from core to implement tile and fuse with scf.forall.
+//===----------------------------------------------------------------------------------===//
+
+}  // namespace mlir::iree_compiler::AMDAIE
