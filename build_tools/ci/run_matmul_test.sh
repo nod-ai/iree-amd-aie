de760b914d5a3551e736c9d3954ce2f62d2ab6ff
Nirvedh Meshram <96096277+nirvedhmeshram@users.noreply.github.com>
2024-02-16 12:39:14 -0600

    Add initial E2E correctness tests (#170)
    These tests are compared against a reference matmul implementation with
    pseudo random initialization. The test sizes are currently limited. In
    upstream IREE similar tests are build using cmake and run using ctest,
    however that is currently not possible for a plugin so the script added
    here is an alternate way of doing it.

--- build_tools/ci/run_matmul_test.sh
+++ build_tools/ci/run_matmul_test.sh
@@ -17,20 +17,37 @@
 #      `iree-e2e-matmul-test` to include support for the runtime HAL
 #      driver/device you wish to test.
 #   2. Update the paths in this script or specify them via environment variables
-#   3. Run: `./run_matmul_tests.sh <peano_install_path> <mlir_aie_install_path> <vitis_path>`
+#   3. Run: `./run_matmul_tests.sh <output_dir_path> <iree_install_path>`
 
-set -euo pipefail
+set -euox pipefail
 
+MLIR_AIE_INSTALL=`realpath .venv/lib/python3.10/site-packages/mlir_aie`
 THIS_DIR="$(cd $(dirname $0) && pwd)"
 ROOT_DIR="$(cd $THIS_DIR/../.. && pwd)"
 
-OUTPUT_DIR="${IREE_MATMUL_BUILD_DIR:-build-matmul}"
+OUTPUT_DIR=`realpath "$1"`
+IREE_INSTALL_DIR="$2"
+if [ -d "${IREE_INSTALL_DIR}/tools" ]; then
+    IREE_INSTALL_TOOLS=`realpath "${IREE_INSTALL_DIR}/tools"`
+fi
+if [ -d "${IREE_INSTALL_DIR}/bin" ]; then
+    IREE_INSTALL_BIN=`realpath "${IREE_INSTALL_DIR}/bin"`
+fi
+
 GENERATOR="${ROOT_DIR}/tests/matmul/generate_e2e_matmul_tests.py"
 IREE_PYTHON3_EXECUTABLE="${IREE_PYTHON3_EXECUTABLE:-python3}"
 
-IREE_INSTALL_BIN="${IREE_INSTALL_BIN:-${ROOT_DIR}/../iree-build/tools/}"
 IREE_COMPILE_EXE="${IREE_INSTALL_BIN}/iree-compile"
-TEST_RUNNER="${IREE_INSTALL_BIN}/iree-e2e-matmul-test"
+TEST_RUNNER="${IREE_INSTALL_TOOLS}/iree-e2e-matmul-test"
+
+XRT_DIR=/opt/xilinx/xrt
+PEANO=/opt/llvm-aie
+VITIS=/opt/Xilinx/Vitis/2023.2
+# An alternate to a full vitis install, will work here but not for a full build of mlir-aie
+# https://riallto.ai/install-riallto.html#install-riallto
+# VITIS=/opt/Riallto/Vitis/2023.1
+
+source $XRT_DIR/setup.sh
 
 ###############################################################################
 # Setup and checking for dependencies                                         #
@@ -39,6 +56,7 @@ TEST_RUNNER="${IREE_INSTALL_BIN}/iree-e2e-matmul-test"
 echo "Python version: $("${IREE_PYTHON3_EXECUTABLE}" --version)"
 echo "iree-compile version: $("${IREE_COMPILE_EXE}" --version)"
 mkdir -p ${OUTPUT_DIR}
+cd ${OUTPUT_DIR}
 
 ###############################################################################
 # Define helper function                                                      #
@@ -120,11 +138,26 @@ function run_matmul_test() {
       --iree-amd-aie-peano-install-dir=${peano_install_path} \
       --iree-amd-aie-mlir-aie-install-dir=${mlir_aie_install_path} \
       --iree-amd-aie-vitis-install-dir=${vitis_path} \
+      --iree-hal-dump-executable-files-to=$PWD \
       -o "${OUTPUT_DIR}/${name}_matmuls.vmfb"
   ${IREE_COMPILE_EXE} \
       "${OUTPUT_DIR}/${name}_calls.mlir" \
       --iree-hal-target-backends=${target_backend} \
       -o "${OUTPUT_DIR}/${name}_calls.vmfb"
+
+  # Extract function names from the mlir file
+  function_names=$(grep -oP '@\K\S+(?=\()' ${OUTPUT_DIR}/${name}_matmuls.mlir)
+
+  # Iterate over each function name and sign the corresponding XCLBIN
+  for func_name in $function_names; do
+    # Define the XCLBIN variable
+    XCLBIN="module_${func_name}_dispatch_0_amdaie_xclbin_fb.xclbin"
+    # Ensure unique file name
+    XCLBIN_UNIQ="github.${GITHUB_RUN_ID}.${GITHUB_RUN_ATTEMPT}.${XCLBIN}"
+    cp "${XCLBIN}" "${XCLBIN_UNIQ}"
+    # Deploy firmware
+    sudo $XRT_DIR/amdxdna/setup_xclbin_firmware.sh -dev Phoenix -xclbin "${XCLBIN_UNIQ}"
+  done
 
   echo "**** Running '${name}' matmul tests ****"
   echo ""
@@ -148,6 +181,17 @@ run_matmul_test \
     --shapes "small" \
     --target_backend "amd-aie" \
     --device "xrt" \
-    --peano_install_path "$1" \
-    --mlir_aie_install_path "$2" \
-    --vitis_path  "$3"
+    --peano_install_path "${PEANO}" \
+    --mlir_aie_install_path "${MLIR_AIE_INSTALL}" \
+    --vitis_path  "${VITIS}" \
+
+run_matmul_test \
+    --name "matmul_i32_i32_large_amd-aie_xrt" \
+    --lhs_rhs_type "i32" \
+    --acc_type "i32" \
+    --shapes "large" \
+    --target_backend "amd-aie" \
+    --device "xrt" \
+    --peano_install_path "${PEANO}" \
+    --mlir_aie_install_path "${MLIR_AIE_INSTALL}" \
+    --vitis_path  "${VITIS}" \
