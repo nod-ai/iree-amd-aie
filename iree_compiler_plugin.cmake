279dc78367011babcba8668b72863d65d8537455
Vivian <vivian.zhang@amd.com>
2024-03-08 11:07:18 -0800

    Add pad-pack and loop peel based transform dialect (#190)

--- iree_compiler_plugin.cmake
+++ iree_compiler_plugin.cmake
@@ -12,3 +12,4 @@ add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/compiler/plugins/target/AMD-AIE targe
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/tools/plugins AMD-AIE/tools)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/tests/samples AMD-AIE/tests/samples)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/tests/OPT/failing_tests AMD-AIE/tests/OPT/failing_tests)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/tests/transform_dialect AMD-AIE/tests/transform_dialect)
