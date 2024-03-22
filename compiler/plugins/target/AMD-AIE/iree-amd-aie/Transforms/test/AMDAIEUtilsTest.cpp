3153b32dd915ca09dd8e35b994faf1127306231a
James Newling <james.newling@gmail.com>
2024-03-22 09:23:20 -0700

    Improved findLargestFactor, with gtest  (#240)
    1) first unit tests for C++ code (googletest) 
    2) new algorithm for findLargestFactor

---
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/test/AMDAIEUtilsTest.cpp
@@ -0,0 +1,58 @@
+#include "gtest/gtest.h"
+
+#include "iree-amd-aie/Transforms/AMDAIEUtils.h"
+
+namespace {
+
+using namespace mlir::iree_compiler::AMDAIE;
+
+TEST(IntegerMatmulInstructionSizeTest, FailsWithStrangeShape) {
+  std::array<uint32_t, 3> failValue{3, 5, 7};
+  EXPECT_EQ(getAIEIntegerMatmulInstructionSize(12, 13, 14).value_or(failValue),
+            failValue);
+}
+
+TEST(FindLargestFactorTest, Test0) {
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 1), 1);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 2), 2);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 3), 3);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 4), 3);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 5), 3);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 6), 6);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 7), 6);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 8), 6);
+
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 5, /* max = */ 6), 5);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 6, /* max = */ 6), 6);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 7, /* max = */ 6), 1);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 8, /* max = */ 6), 4);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 9, /* max = */ 6), 3);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 10, /* max = */ 6), 5);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 11, /* max = */ 6), 1);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 12, /* max = */ 6), 6);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 13, /* max = */ 6), 1);
+
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 12, /* max = */ 5), 4);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 12, /* max = */ 4), 4);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 12, /* max = */ 3), 3);
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 12, /* max = */ 2), 2);
+
+  // Simple algo iterates 10'000 -> 7'500 in steps of 1 (2'500 iterations)
+  // Current sqrt bounding algo finds and returns 7'500 in 1 iteration.
+  EXPECT_EQ(detail::findLargestFactor(/* num = */ 15'000, /* max = */ 10'000),
+            7'500);
+
+  // Simple algorithms runs ~10'000 iterations
+  // Current sqrt bounding algo runs ~100 iterations
+  int firstPrimeAbove1e5 = 10'037;
+  EXPECT_EQ(
+      detail::findLargestFactor(firstPrimeAbove1e5, firstPrimeAbove1e5 - 1), 1);
+}
+
+}  // namespace
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
