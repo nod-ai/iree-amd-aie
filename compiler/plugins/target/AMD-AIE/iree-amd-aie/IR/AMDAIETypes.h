525cd77c96ddde920973676264dbe5eb1f349f33
Jorn Tuyls <jtuyls@users.noreply.github.com>
2024-04-15 21:37:35 +0200

    Move LogicalObjectFifoType to tablegen (#270)

--- compiler/plugins/target/AMD-AIE/iree-amd-aie/IR/AMDAIETypes.h
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/IR/AMDAIETypes.h
@@ -4,28 +4,20 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifndef IREE_COMPILER_AMDAIE_OPS_H_
-#define IREE_COMPILER_AMDAIE_OPS_H_
+#ifndef IREE_COMPILER_AMDAIE_TYPES_H_
+#define IREE_COMPILER_AMDAIE_TYPES_H_
 
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
-#include "mlir/Interfaces/CopyOpInterface.h"
 
 #include "iree-amd-aie/IR/AMDAIEAttrs.h"
 #include "iree-amd-aie/IR/AMDAIEDmaOpInterface.h"
-#include "iree-amd-aie/IR/AMDAIETypes.h"
 
 // clang-format off
-#include "iree-amd-aie/IR/AMDAIEAttrs.h"
-#include "iree-amd-aie/IR/AMDAIEDialect.h"
-#define GET_OP_CLASSES
-#include "iree-amd-aie/IR/AMDAIEOps.h.inc"
+#define GET_TYPEDEF_CLASSES
+#include "iree-amd-aie/IR/AMDAIETypes.h.inc"
 // clang-format on
 
-namespace mlir::iree_compiler::AMDAIE {
-
-}  // namespace mlir::iree_compiler::AMDAIE
-
-#endif  // IREE_COMPILER_AMDAIE_OPS_H_
+#endif  // IREE_COMPILER_AMDAIE_TYPES_H_
