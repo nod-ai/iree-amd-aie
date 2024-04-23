bfdfeb32e5854f651f8a7919726f5d705e13d3a4
erweiw <erwei.wang@amd.com>
2024-04-22 21:36:58 -0600

    Add ci test

--- build_tools/ci/run_matmul_test.sh
+++ build_tools/ci/run_matmul_test.sh
@@ -532,3 +532,9 @@ run_matmul_test \
     --acc_type "i32" \
     --m "64"  --n "64" --k "64"
 
+run_matmul_test \
+    --name_prefix "large" \
+    --lhs_rhs_type "bf16" \
+    --acc_type "f32" \
+    --m "2304"  --n "2304" --k "2304"
+
