48fcf778d7f8e3ab912072729c0aa305283aa4bd
erweiw <erwei.wang@amd.com>
2024-04-23 18:18:59 -0600

    Add test

--- build_tools/ci/run_matmul_test.sh
+++ build_tools/ci/run_matmul_test.sh
@@ -538,3 +538,10 @@ run_matmul_test \
     --acc_type "f32" \
     --m "128"  --n "128" --k "2304"
 
+run_matmul_test \
+    --name_prefix "packPeel" \
+    --pipeline "pack-peel" \
+    --lhs_rhs_type "i32" \
+    --acc_type "i32" \
+    --m "64"  --n "64" --k "160"
+
