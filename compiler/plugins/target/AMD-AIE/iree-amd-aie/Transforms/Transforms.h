a9d7a270da834b405f523097d0856434b2a8ea6e
Jorn Tuyls <jtuyls@users.noreply.github.com>
2024-05-13 22:39:27 +0200

    Add a pass to unroll loops in control code regions (#343)
    This PR adds a utility pass to unroll scf.for and scf.forall inside
    control code regions. This is used to avoid loops inside control code
    regions as the loops can't be executed by the uController right now.

--- compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/Transforms.h
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/Transforms.h
@@ -7,14 +7,20 @@
 #ifndef IREE_AMD_AIE_TRANSFORMS_AMDAIETRANSFORMS_H_
 #define IREE_AMD_AIE_TRANSFORMS_AMDAIETRANSFORMS_H_
 
-#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "iree-amd-aie/IR/AMDAIEOps.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 
 namespace mlir::iree_compiler::AMDAIE {
 
+/// Unroll the loops within the control code regions.
+LogicalResult controlCodeLoopUnroll(RewriterBase &rewriter,
+                                    AMDAIE::ControlCodeOp controlCodeOp);
+
 /// Hoist an affine apply op on a scf.for op's induction variable
 /// TODO(jornt): Can we generalize this to go into upstream?
-LogicalResult hoistForAffineApplyOp(RewriterBase &rewriter, affine::AffineApplyOp applyOp);
+LogicalResult hoistForAffineApplyOp(RewriterBase &rewriter,
+                                    affine::AffineApplyOp applyOp);
 
 }  // namespace mlir::iree_compiler::AMDAIE
 
