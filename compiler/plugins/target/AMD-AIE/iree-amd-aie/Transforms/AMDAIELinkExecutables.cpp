84b1f506ef78411dcd3d5715ca7f4533a5341b7d
Nirvedh Meshram <nirvedh@gmail.com>
2024-06-04 09:43:28 -0600

    correct file name and address a missed reviwer comments

--- compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/AMDAIELinkExecutables.cpp
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/AMDAIELinkExecutables.cpp
@@ -42,6 +42,9 @@ struct AMDAIELinkExecutablesPass
         OpBuilder::atBlockBegin(&linkedExecutableOp.getBlock());
 
     // Gather all unique executable targets - we may have multiple.
+    // However, in AIE currently we have only one unique target but we still
+    // do this for the same of future proofing and ease of implementation
+    // as we can use the upstream gatherExecutableTargets utility this way.
     SetVector<IREE::HAL::ExecutableTargetAttr> executableTargetAttrs =
         gatherExecutableTargets(sourceExecutableOps);
     for (auto [index, attr] : llvm::enumerate(executableTargetAttrs)) {
