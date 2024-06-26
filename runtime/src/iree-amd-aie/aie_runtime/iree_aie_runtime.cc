4bf2501fd627b286a9925d239eba368766501c5e
max <maksim.levental@gmail.com>
2024-06-26 04:30:15 -0500

    [AMDAIE] replace target model with device model via aie-rt

--- runtime/src/iree-amd-aie/aie_runtime/iree_aie_runtime.cc
+++ runtime/src/iree-amd-aie/aie_runtime/iree_aie_runtime.cc
@@ -92,3 +92,283 @@ STRINGIFY_2TUPLE_STRUCT(XAie_Packet, PktId, PktType)
 
 BOTH_OSTREAM_OPS_FORALL_TYPES(OSTREAM_OP, BOTH_OSTREAM_OP)
 #undef OSTREAM_OP
+
+bool isInternal(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol,
+                uint8_t dstRow) {
+  return srcCol == dstCol && srcRow == dstRow;
+}
+
+bool isWest(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol, uint8_t dstRow) {
+  return srcCol == dstCol + 1 && srcRow == dstRow;
+}
+
+bool isEast(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol, uint8_t dstRow) {
+  return srcCol == dstCol - 1 && srcRow == dstRow;
+}
+
+bool isNorth(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol, uint8_t dstRow) {
+  return srcCol == dstCol && srcRow == dstRow - 1;
+}
+
+bool isSouth(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol, uint8_t dstRow) {
+  return srcCol == dstCol && srcRow == dstRow + 1;
+}
+
+AMDAIENPUDeviceModel::AMDAIENPUDeviceModel(
+    uint8_t aieGen, uint64_t baseAddr, uint8_t colShift, uint8_t rowShift,
+    uint8_t nColumns, uint8_t nRows, uint8_t memTileRowStart,
+    uint8_t nMemTileRows, uint8_t nShimTileRows, uint8_t partitionStartCol,
+    uint8_t partitionNumCols, bool aieSim, bool xaieDebug)
+    : configPtr{.AieGen = aieGen,
+                .BaseAddr = baseAddr,
+                .ColShift = colShift,
+                .RowShift = rowShift,
+                .NumRows = nRows,
+                .NumCols = nColumns,
+                .ShimRowNum = 0,
+                .MemTileRowStart = memTileRowStart,
+                .MemTileNumRows = nMemTileRows,
+                .AieTileRowStart =
+                    static_cast<uint8_t>(memTileRowStart + nMemTileRows),
+                .AieTileNumRows =
+                    static_cast<uint8_t>(nRows - nMemTileRows - nShimTileRows),
+                .PartProp = {},
+                .Backend = XAIE_IO_BACKEND_CDO},
+      devInst{} {
+  TRY_XAIE_API_FATAL_ERROR(XAie_SetupPartitionConfig, &devInst,
+                           XAIE_PARTITION_BASE_ADDR, partitionStartCol,
+                           partitionNumCols);
+  TRY_XAIE_API_FATAL_ERROR(XAie_CfgInitialize, &devInst, &configPtr);
+  if (aieSim) {
+    TRY_XAIE_API_FATAL_ERROR(XAie_SetIOBackend, &devInst, XAIE_IO_BACKEND_SIM);
+  } else if (xaieDebug)
+    TRY_XAIE_API_FATAL_ERROR(XAie_SetIOBackend, &devInst,
+                             XAIE_IO_BACKEND_DEBUG);
+  else
+    TRY_XAIE_API_FATAL_ERROR(XAie_SetIOBackend, &devInst, XAIE_IO_BACKEND_CDO);
+
+  TRY_XAIE_API_FATAL_ERROR(XAie_UpdateNpiAddr, &devInst, NPI_ADDR);
+}
+
+int AMDAIENPUDeviceModel::rows() const { return configPtr.NumRows; }
+
+int AMDAIENPUDeviceModel::columns() const { return configPtr.NumCols; }
+
+uint32_t AMDAIENPUDeviceModel::getNumMemTileRows() const {
+  return configPtr.MemTileNumRows;
+}
+
+// TODO(max): these are buried somewhere in aie-rt...
+uint32_t AMDAIENPUDeviceModel::getMemSouthBaseAddress() { return 0x00040000; }
+uint32_t AMDAIENPUDeviceModel::getMemWestBaseAddress() { return 0x00050000; }
+uint32_t AMDAIENPUDeviceModel::getMemNorthBaseAddress() { return 0x00060000; }
+uint32_t AMDAIENPUDeviceModel::getMemEastBaseAddress() { return 0x00070000; }
+
+bool AMDAIENPUDeviceModel::isCoreTile(uint8_t col, uint8_t row) {
+  return devInst.DevOps->GetTTypefromLoc(&devInst, {.Row = row, .Col = col}) ==
+         XAIEGBL_TILE_TYPE_AIETILE;
+}
+
+bool AMDAIENPUDeviceModel::isMemTile(uint8_t col, uint8_t row) {
+  return devInst.DevOps->GetTTypefromLoc(&devInst, {.Row = row, .Col = col}) ==
+         XAIEGBL_TILE_TYPE_MEMTILE;
+}
+
+bool AMDAIENPUDeviceModel::isShimNOCTile(uint8_t col, uint8_t row) {
+  return devInst.DevOps->GetTTypefromLoc(&devInst, {.Row = row, .Col = col}) ==
+         XAIEGBL_TILE_TYPE_SHIMNOC;
+}
+
+bool AMDAIENPUDeviceModel::isShimPLTile(uint8_t col, uint8_t row) {
+  return devInst.DevOps->GetTTypefromLoc(&devInst, {.Row = row, .Col = col}) ==
+         XAIEGBL_TILE_TYPE_SHIMPL;
+}
+
+uint32_t AMDAIENPUDeviceModel::getNumLocks(uint8_t col, uint8_t row) {
+  uint8_t tileType =
+      devInst.DevOps->GetTTypefromLoc(&devInst, {.Row = row, .Col = col});
+  assert(tileType != XAIEGBL_TILE_TYPE_MAX && "invalid tile");
+  return devInst.DevProp.DevMod[tileType].LockMod->NumLocks;
+}
+
+uint32_t AMDAIENPUDeviceModel::getNumBDs(uint8_t col, uint8_t row) {
+  uint8_t tileType =
+      devInst.DevOps->GetTTypefromLoc(&devInst, {.Row = row, .Col = col});
+  assert(tileType != XAIEGBL_TILE_TYPE_MAX && "invalid tile");
+  const XAie_DmaMod *dmaMod = devInst.DevProp.DevMod[tileType].DmaMod;
+  return dmaMod->NumBds;
+}
+
+std::optional<TileLoc> AMDAIENPUDeviceModel::getMemWest(TileLoc src) {
+  XAie_LocType ret = XAie_TileLoc(src.col - 1, src.row);
+  if (devInst.DevOps->GetTTypefromLoc(&devInst, ret) == XAIEGBL_TILE_TYPE_MAX)
+    return std::nullopt;
+  return ret;
+}
+
+std::optional<TileLoc> AMDAIENPUDeviceModel::getMemEast(TileLoc src) {
+  // east is self
+  return src;
+}
+
+std::optional<TileLoc> AMDAIENPUDeviceModel::getMemNorth(TileLoc src) {
+  XAie_LocType ret = XAie_TileLoc(src.col, src.row + 1);
+  if (devInst.DevOps->GetTTypefromLoc(&devInst, ret) == XAIEGBL_TILE_TYPE_MAX)
+    return std::nullopt;
+  return ret;
+}
+
+std::optional<TileLoc> AMDAIENPUDeviceModel::getMemSouth(TileLoc src) {
+  XAie_LocType ret = XAie_TileLoc(src.col, src.row - 1);
+  auto tt = devInst.DevOps->GetTTypefromLoc(&devInst, ret);
+  // The first row doesn't have a tile memory south
+  // Memtiles don't have memory adjacency to neighboring core tiles.
+  if (tt == XAIEGBL_TILE_TYPE_MAX || ret.Row == 0 ||
+      tt == XAIEGBL_TILE_TYPE_MEMTILE)
+    return std::nullopt;
+  return ret;
+}
+
+// I don't know why you don't need to check for memtile or core tile here
+// but this repros what mlir-aie does
+bool AMDAIENPUDeviceModel::hasMemWest(uint8_t srcCol, uint8_t srcRow,
+                                      uint8_t dstCol, uint8_t dstRow) {
+  return isWest(srcCol, srcRow, dstCol, dstRow);
+}
+
+bool AMDAIENPUDeviceModel::hasMemEast(uint8_t srcCol, uint8_t srcRow,
+                                      uint8_t dstCol, uint8_t dstRow) {
+  return isInternal(srcCol, srcRow, dstCol, dstRow);
+}
+
+bool AMDAIENPUDeviceModel::hasMemNorth(uint8_t srcCol, uint8_t srcRow,
+                                       uint8_t dstCol, uint8_t dstRow) {
+  return isNorth(srcCol, srcRow, dstCol, dstRow);
+}
+
+bool AMDAIENPUDeviceModel::hasMemSouth(uint8_t srcCol, uint8_t srcRow,
+                                       uint8_t dstCol, uint8_t dstRow) {
+  return isSouth(srcCol, srcRow, dstCol, dstRow);
+}
+
+uint32_t AMDAIENPUDeviceModel::getLocalMemorySize(uint8_t col, uint8_t row) {
+  auto tileLoc = XAie_TileLoc(col, row);
+  uint8_t tileType = devInst.DevOps->GetTTypefromLoc(&devInst, tileLoc);
+  assert(tileType != XAIEGBL_TILE_TYPE_MAX && "invalid tile");
+  return devInst.DevProp.DevMod[tileType].CoreMod->DataMemSize;
+}
+
+uint32_t AMDAIENPUDeviceModel::getMemInternalBaseAddress() {
+  return getMemEastBaseAddress();
+}
+
+uint32_t AMDAIENPUDeviceModel::getMemTileSize(uint8_t col, uint8_t row) {
+  auto tileLoc = XAie_TileLoc(col, row);
+  uint8_t tileType = devInst.DevOps->GetTTypefromLoc(&devInst, tileLoc);
+  assert(tileType != XAIEGBL_TILE_TYPE_MAX && "invalid tile");
+  return devInst.DevProp.DevMod[tileType].MemMod->Size;
+}
+
+bool AMDAIENPUDeviceModel::hasLegalMemAffinity(uint8_t coreCol, uint8_t coreRow,
+                                               uint8_t memCol, uint8_t memRow) {
+  bool isMemWest = hasMemWest(coreCol, coreRow, memCol, memRow);
+  bool isMemEast = hasMemEast(coreCol, coreRow, memCol, memRow);
+  bool isMemNorth = hasMemNorth(coreCol, coreRow, memCol, memRow);
+  bool isMemSouth = hasMemSouth(coreCol, coreRow, memCol, memRow);
+
+  if (isMemTile(coreCol, coreRow))
+    return isEast(coreCol, coreRow, memCol, memRow) ||
+           isInternal(coreCol, coreRow, memCol, memRow) ||
+           isWest(coreCol, coreRow, memCol, memRow);
+  return (isMemSouth && !isMemTile(memCol, memRow)) || isMemNorth ||
+         isMemWest || isMemEast;
+}
+
+bool AMDAIENPUDeviceModel::isLegalMemtileConnection(uint8_t col, uint8_t row,
+                                                    StrmSwPortType srcBundle,
+                                                    uint8_t srcChan,
+                                                    StrmSwPortType dstBundle,
+                                                    uint8_t dstChan) {
+  // this isn't correct but for agreement with mlir-aie...
+  if (srcBundle == dstBundle and srcBundle != DMA) return true;
+  assert(isMemTile(col, row) && "expected memtile");
+  auto tileLoc = XAie_TileLoc(col, row);
+  uint8_t tileType = devInst.DevOps->GetTTypefromLoc(&devInst, tileLoc);
+  assert(tileType != XAIEGBL_TILE_TYPE_MAX && "invalid tile");
+  const XAie_StrmMod *strmMod = devInst.DevProp.DevMod[tileType].StrmSw;
+  AieRC RC = strmMod->PortVerify(/*slave*/ srcBundle, srcChan,
+                                 /*master*/ dstBundle, dstChan);
+  if (RC != XAIE_OK) {
+    LLVM_DEBUG(llvm::dbgs() << "PortVerify failed with " << RC);
+    LLVM_DEBUG(SHOW_ARGS(llvm::dbgs(), col, row, srcBundle, srcChan, dstBundle,
+                         dstChan));
+    return false;
+  }
+  return true;
+}
+
+// source <-> slave and dest <-> master
+uint32_t AMDAIENPUDeviceModel::getNumSourceSwitchboxConnections(
+    uint8_t col, uint8_t row, StrmSwPortType bundle) {
+  // not sure if this makes sense but agrees with mlir-aie
+  if ((bundle == NORTH && row == rows() - 1) || (bundle == WEST && col == 0) ||
+      (bundle == EAST && col == columns() - 1))
+    return 0;
+  uint8_t tileType =
+      devInst.DevOps->GetTTypefromLoc(&devInst, {.Row = row, .Col = col});
+  assert(tileType != XAIEGBL_TILE_TYPE_MAX && "invalid tile");
+  const XAie_StrmMod *strmMod = devInst.DevProp.DevMod[tileType].StrmSw;
+  return strmMod->SlvConfig[bundle].NumPorts;
+}
+
+uint32_t AMDAIENPUDeviceModel::getNumDestSwitchboxConnections(
+    uint8_t col, uint8_t row, StrmSwPortType bundle) {
+  // not sure if this makes sense but agrees with mlir-aie
+  if ((bundle == NORTH && row == rows() - 1) || (bundle == WEST && col == 0) ||
+      (bundle == EAST && col == columns() - 1))
+    return 0;
+
+  uint8_t tileType =
+      devInst.DevOps->GetTTypefromLoc(&devInst, {.Row = row, .Col = col});
+  assert(tileType != XAIEGBL_TILE_TYPE_MAX && "invalid tile");
+  const XAie_StrmMod *strmMod = devInst.DevProp.DevMod[tileType].StrmSw;
+  return strmMod->MstrConfig[bundle].NumPorts;
+}
+
+struct AMDAIENPUDeviceModel mlir::iree_compiler::AMDAIE::getDeviceModel(
+    AMDAIEDevice device) {
+  switch (device) {
+    case AMDAIEDevice::xcvc1902:
+      return AMDAIENPUDeviceModel(XAIE_DEV_GEN_AIE, XAIE1_BASE_ADDR,
+                                  XAIE1_COL_SHIFT, XAIE1_ROW_SHIFT,
+                                  /*numCols*/ 50, /*numRows*/ 9,
+                                  /*memTileRowStart*/ 0, /*nMemTileRows*/ 0,
+                                  /*nShimTileRows*/ 0, /*partitionStartCol*/ 0,
+                                  /*partitionNumCols*/ 50);
+    case AMDAIEDevice::xcve2302:
+      return AMDAIENPUDeviceModel(XAIE_DEV_GEN_AIEML, XAIE2_BASE_ADDR,
+                                  XAIE2_COL_SHIFT, XAIE2_ROW_SHIFT,
+                                  /*numCols*/ 17, /*numRows*/ 4,
+                                  /*memTileRowStart*/ 1, /*nMemTileRows*/ 1,
+                                  /*nShimTileRows*/ 1, /*partitionStartCol*/ 0,
+                                  /*partitionNumCols*/ 17);
+    case AMDAIEDevice::xcve2802:
+      return AMDAIENPUDeviceModel(XAIE_DEV_GEN_AIEML, XAIE2_BASE_ADDR,
+                                  XAIE2_COL_SHIFT, XAIE2_ROW_SHIFT,
+                                  /*numCols*/ 38, /*numRows*/ 11,
+                                  /*memTileRowStart*/ 2, /*nMemTileRows*/ 1,
+                                  /*nShimTileRows*/ 1, /*partitionStartCol*/ 0,
+                                  /*partitionNumCols*/ 38);
+    case AMDAIEDevice::npu:
+    case AMDAIEDevice::npu1_1col:
+    case AMDAIEDevice::npu1_2col:
+    case AMDAIEDevice::npu1_3col:
+    case AMDAIEDevice::npu1_4col:
+      return AMDAIENPUDeviceModel(XAIE_DEV_GEN_AIEML, XAIE2_BASE_ADDR,
+                                  XAIE2_COL_SHIFT, XAIE2_ROW_SHIFT,
+                                  /*numCols*/ 5, /*numRows*/ 6,
+                                  /*memTileRowStart*/ 1, /*nMemTileRows*/ 1,
+                                  /*nShimTileRows*/ 1, /*partitionStartCol*/ 0,
+                                  /*partitionNumCols*/ 5);
+  }
+}
--- runtime/src/iree-amd-aie/aie_runtime/iree_aie_runtime.h
+++ runtime/src/iree-amd-aie/aie_runtime/iree_aie_runtime.h
@@ -43,6 +43,28 @@ void configureHeader();
 void insertNoOpCommand(unsigned int numPadBytes);
 }
 
+#define s8
+#define u8
+#define u16
+#define s32
+#define u32
+#define u64
+
+#define XAIE2_BASE_ADDR 0x40000000
+#define XAIE1_BASE_ADDR 0x20000000000
+#define XAIE2_COL_SHIFT 25
+#define XAIE2_ROW_SHIFT 20
+#define XAIE1_COL_SHIFT 23
+#define XAIE1_ROW_SHIFT 18
+#define XAIE_SHIM_ROW 0
+#define XAIE_MEM_TILE_ROW_START 1
+#define XAIE_PARTITION_BASE_ADDR 0x0
+
+#define NPI_ADDR 0x0
+#define NUM_LOCKS 16
+#define MEM_TILE_LOCK_ID_INCR 64
+#define BASE_ADDR_A_INCR 0x80000
+
 #define OSTREAM_OP(O_TYPE, TYPE) O_TYPE &operator<<(O_TYPE &os, const TYPE &s);
 
 namespace mlir::iree_compiler::AMDAIE {
@@ -131,5 +153,120 @@ static_assert(XAIE_OK == 0);
       return failure();                                                 \
     }                                                                   \
   } while (0)
+
+struct TileLoc {
+  // friend definition (will define the function as a non-member function in the
+  // namespace surrounding the class).
+  friend std::ostream &operator<<(std::ostream &os, const TileLoc &s) {
+    os << "TileLoc(" << s.col << ", " << s.row << ")";
+    return os;
+  }
+
+  friend std::string to_string(const TileLoc &s) {
+    std::ostringstream ss;
+    ss << s;
+    return ss.str();
+  }
+
+  friend llvm::raw_ostream &operator<<(llvm::raw_ostream &os,
+                                       const TileLoc &s) {
+    os << to_string(s);
+    return os;
+  }
+
+  inline bool operator<(const TileLoc &rhs) const {
+    return std::tie(col, row) < std::tie(rhs.col, rhs.row);
+  }
+
+  bool operator==(const TileLoc &rhs) const {
+    return std::tie(col, row) == std::tie(rhs.col, rhs.row);
+  }
+
+  bool operator!=(const TileLoc &rhs) const { return !(*this == rhs); }
+
+  operator XAie_LocType() const { return XAie_TileLoc(col, row); }
+  TileLoc(XAie_LocType loc) : col(loc.Col), row(loc.Row) {}
+  TileLoc(int col, int row) : col(col), row(row) {}
+
+  int col, row;
+};
+
+enum class AMDAIEDevice : uint32_t {
+  xcvc1902 = 1,
+  xcve2302 = 2,
+  xcve2802 = 3,
+  npu = 4,
+  npu1 = 4,
+  npu1_1col = 5,
+  npu1_2col = 6,
+  npu1_3col = 7,
+  npu1_4col = 8,
+};
+
+struct AMDAIENPUDeviceModel {
+  XAie_Config configPtr;
+  XAie_DevInst devInst;
+
+  explicit AMDAIENPUDeviceModel(
+      uint8_t aieGen, uint64_t baseAddr, uint8_t colShift, uint8_t rowShift,
+      uint8_t nColumns, uint8_t, uint8_t memTileRowStart, uint8_t nMemTileRows,
+      uint8_t nShimTileRows, uint8_t partitionStartCol,
+      uint8_t partitionNumCols, bool aieSim = false, bool xaieDebug = false);
+
+  int rows() const;
+  int columns() const;
+  uint32_t getNumMemTileRows() const;
+
+  bool isCoreTile(uint8_t col, uint8_t row);
+  bool isMemTile(uint8_t col, uint8_t row);
+  bool isShimNOCTile(uint8_t col, uint8_t row);
+  bool isShimPLTile(uint8_t col, uint8_t row);
+
+  uint32_t getNumLocks(uint8_t col, uint8_t row);
+
+  std::optional<TileLoc> getMemWest(TileLoc src);
+  std::optional<TileLoc> getMemEast(TileLoc src);
+  std::optional<TileLoc> getMemNorth(TileLoc src);
+  std::optional<TileLoc> getMemSouth(TileLoc src);
+
+  bool hasMemWest(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol,
+                  uint8_t dstRow);
+  bool hasMemEast(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol,
+                  uint8_t dstRow);
+  bool hasMemNorth(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol,
+                   uint8_t dstRow);
+  bool hasMemSouth(uint8_t srcCol, uint8_t srcRow, uint8_t dstCol,
+                   uint8_t dstRow);
+  /// Return true if core can access the memory in mem
+  bool hasLegalMemAffinity(uint8_t coreCol, uint8_t coreRow, uint8_t memCol,
+                           uint8_t memRow);
+
+  uint32_t getMemInternalBaseAddress();
+  uint32_t getMemSouthBaseAddress();
+  uint32_t getMemWestBaseAddress();
+  uint32_t getMemNorthBaseAddress();
+  uint32_t getMemEastBaseAddress();
+  uint32_t getLocalMemorySize(uint8_t col, uint8_t row);
+  uint32_t getMemTileSize(uint8_t col, uint8_t row);
+
+  uint32_t getNumBDs(uint8_t col, uint8_t row);
+
+  uint32_t getNumSourceSwitchboxConnections(uint8_t col, uint8_t row,
+                                            StrmSwPortType bundle);
+  uint32_t getNumDestSwitchboxConnections(uint8_t col, uint8_t row,
+                                          StrmSwPortType bundle);
+  bool isLegalMemtileConnection(uint8_t col, uint8_t row,
+                                StrmSwPortType srcBundle, uint8_t srcChan,
+                                StrmSwPortType dstBundle, uint8_t dstChan);
+};
+
+namespace mlir::iree_compiler::AMDAIE {
+
+struct AMDAIENPUDeviceModel getDeviceModel(AMDAIEDevice device);
+
+}  // namespace mlir::iree_compiler::AMDAIE
+
+StrmSwPortType getConnectingStrmSwPortType(StrmSwPortType dir);
+std::string stringifyStrmSwPortType(StrmSwPortType val);
 
 #endif  // IREE_AIE_RUNTIME_H
