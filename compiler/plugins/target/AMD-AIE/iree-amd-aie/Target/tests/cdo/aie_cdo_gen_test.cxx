22107961aa29abfc00f9c123b9089ff90dc4d04c
Maksim Levental <maksim.levental@gmail.com>
2024-07-05 09:55:25 -0500

    Use upstream Xilinx/aie-rt (#499)

---
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Target/tests/cdo/aie_cdo_gen_test.cxx
@@ -0,0 +1,61 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <string>
+
+#include "aie/Dialect/AIE/IR/AIEDialect.h"
+#include "aie/Dialect/AIEX/IR/AIEXDialect.h"
+#include "iree-amd-aie/Target/AMDAIETargets.h"
+#include "iree-amd-aie/Target/XCLBinGen.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Path.h"
+#include "mlir/Dialect/Vector/IR/VectorOps.h"
+#include "mlir/Parser/Parser.h"
+
+using namespace mlir;
+using namespace mlir::iree_compiler::AMDAIE;
+
+void registerDialects(DialectRegistry &registry) {
+  registry.insert<xilinx::AIE::AIEDialect>();
+  registry.insert<xilinx::AIEX::AIEXDialect>();
+  registry.insert<func::FuncDialect>();
+  registry.insert<scf::SCFDialect>();
+  registry.insert<cf::ControlFlowDialect>();
+  registry.insert<arith::ArithDialect>();
+  registry.insert<mlir::vector::VectorDialect>();
+  registry.insert<memref::MemRefDialect>();
+  registry.insert<LLVM::LLVMDialect>();
+}
+
+int main(int argc, char **argv) {
+  llvm::StringRef mlirAbsPath(argv[1]);
+  llvm::StringRef workDir(argv[2]);
+
+
+  DialectRegistry registry;
+  registerDialects(registry);
+  MLIRContext context(registry);
+
+  mlir::ParserConfig parserConfig(&context);
+  auto moduleOp = llvm::cast<ModuleOp>(
+      mlir::parseSourceFile(mlirAbsPath, parserConfig).release());
+  llvm::DebugFlag = true;
+  auto status = AIETranslateToCDODirect(moduleOp, workDir, false, false, false);
+  std::vector<std::string> diagnostics;
+  ScopedDiagnosticHandler handler(moduleOp.getContext(), [&](Diagnostic &d) {
+    llvm::raw_string_ostream(diagnostics.emplace_back())
+        << d.getLocation() << ": " << d;
+  });
+
+  if (failed(status))
+    for (const auto &diagnostic : diagnostics) std::cerr << diagnostic << "\n";
+
+  llvm::DebugFlag = false;
+  status = AIETranslateToCDODirect(moduleOp, workDir, false, false, true);
+  if (failed(status))
+    for (const auto &diagnostic : diagnostics) std::cerr << diagnostic << "\n";
+}
