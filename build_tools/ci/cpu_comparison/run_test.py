d1073056ffa47b295da29c4df3ab0618c2e4f825
Maksim Levental <maksim.levental@gmail.com>
2024-08-19 17:14:04 -0500

    Revert "[WIP] windows e2e"
    This reverts commit 6f7bb6c2155e1bd1255065c4ffa787ea20320ff8.

--- build_tools/ci/cpu_comparison/run_test.py
+++ build_tools/ci/cpu_comparison/run_test.py
@@ -4,7 +4,6 @@
 
 import argparse
 import os
-import platform
 import re
 import subprocess
 import time
@@ -47,10 +46,6 @@ def find_executable(install_dir: Path, executable_name):
         install_dir / "bin",
         install_dir / "tools",
     ]
-
-    if platform.system() == "Windows":
-        executable_name += ".exe"
-
     for directory in search_dirs:
         executable_path = directory / executable_name
         if executable_path.is_file():
