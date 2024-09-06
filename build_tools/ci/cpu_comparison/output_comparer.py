0cfc4705cf57d8e78eded0c60d088a54bd8d3765
James Newling <james.newling@gmail.com>
2024-09-06 08:58:35 -0700

    squash commits, cosmetic improvements

--- build_tools/ci/cpu_comparison/output_comparer.py
+++ build_tools/ci/cpu_comparison/output_comparer.py
@@ -2,65 +2,148 @@ import numpy as np
 import sys
 
 
-def compare(npy_cpu1, npy_aie1, rtol, atol):
+def getNpArrayString(X):
+    """
+    Try to get a string of integers if possible, otherwise use whatever
+    the native element type is.
+    """
+    if (np.abs(X - np.array(X, dtype=np.int32))).sum() == 0:
+        X = np.array(X, dtype=np.int32)
+    return str(X)
 
-    print(f"Running 'compare' with rtol={rtol} and atol={atol}")
 
-    if npy_cpu1.shape != npy_aie1.shape:
-        raise ValueError(
-            "The two outputs have different shapes: {} and {}".format(
-                npy_cpu1.shape, npy_aie1.shape
+class Summarizer:
+    """
+    Class with methods to summarize the differences between two numpy arrays:
+    the baseline values and the AIE output (although could be any two numpy
+    arrays, should rename).
+    """
+
+    def __init__(self, npy_baseline, npy_aie, rtol, atol):
+        self.npy_baseline = npy_baseline
+        self.npy_aie = npy_aie
+        self.rtol = rtol
+        self.atol = atol
+        self.close_mask = np.isclose(
+            self.npy_baseline, self.npy_aie, rtol=rtol, atol=atol
+        )
+        self.not_close_mask = np.logical_not(self.close_mask)
+        self.where_not_close = np.where(self.not_close_mask)
+        self.nb_not_close = len(self.where_not_close[0])
+        self.abs_diff = np.abs(self.npy_baseline - self.npy_aie)
+        self.aie_values_where_not_close = self.npy_aie[self.where_not_close]
+        self.baseline_values_where_not_close = self.npy_baseline[self.where_not_close]
+
+    def getBaseString(self):
+        summary = "Values are not all close, "
+        summary += "here is a summary of the differences:\n\n"
+        summary += (
+            "- number of positions where values are different is {} out of {}\n".format(
+                self.nb_not_close, self.npy_baseline.size
             )
         )
 
-    are_close = np.allclose(npy_cpu1, npy_aie1, rtol=rtol, atol=atol)
+        summary += "- maximum absolute difference: {}\n".format(np.max(self.abs_diff))
+        summary += "- mean absolute difference: {}\n".format(np.mean(self.abs_diff))
+        summary += "- number of zeros in baseline array: {}\n".format(
+            np.sum(self.npy_baseline == 0)
+        )
+        summary += "- number of zeros in AIE array: {}\n".format(
+            np.sum(self.npy_aie == 0)
+        )
+        summary += "- baseline values are in the range [{}, {}]\n".format(
+            np.min(self.npy_baseline), np.max(self.npy_baseline)
+        )
+        summary += "- AIE values are in the range [{}, {}]\n".format(
+            np.min(self.npy_aie), np.max(self.npy_aie)
+        )
+        return summary
+
+    def getDiscrepenciesString(self, max_discrepancies_to_show):
+        nb_discrepencies_to_show = min(max_discrepancies_to_show, self.nb_not_close)
+        summary = f"\n\nDiscrepencies at first {nb_discrepencies_to_show} indices:\n\n"
+        coords = np.array(self.where_not_close).T[0:nb_discrepencies_to_show]
+        coords = str(coords).split("\n")
+        for i in range(nb_discrepencies_to_show):
+            if i != nb_discrepencies_to_show - 1:
+                coords[i] += " "
+            coords[i] = (
+                coords[i]
+                + " | "
+                + "{: <23}".format(self.baseline_values_where_not_close[i])
+                + " | "
+                + "{: <23}".format(self.aie_values_where_not_close[i])
+            )
+        indices = [i for i, c in enumerate(coords[0]) if c == "|"]
+        assert len(indices) == 2
+        barrier_0 = indices[0]
+        barrier_1 = indices[1]
+        header_line = " Index"
+        header_line = header_line + " " * (barrier_0 - len(header_line))
+        header_line = header_line + "| Baseline"
+        header_line = header_line + " " * (barrier_1 - len(header_line))
+        header_line = header_line + "| AIE"
+        coords.insert(0, header_line)
+        underline = ["-" for i in range(len(header_line))]
+        underline[barrier_0] = "+"
+        underline[barrier_1] = "+"
+        underline = "".join(underline)
+        coords.insert(1, underline)
+        coords.append(underline)
+        summary += "\n".join(coords)
+        return summary
+
+    def getSlicesString(self):
+        point = [self.where_not_close[j][0] for j in range(len(self.where_not_close))]
+        start_idxs = []
+        end_idxs = []
+        slices = []
+        for i, s in enumerate(point):
+            start_idx = max(0, s - 3)
+            end_idx = min(self.npy_aie.shape[i], s + 3)
+            start_idxs.append(start_idx)
+            end_idxs.append(end_idx)
+            slices.append(slice(start_idx, end_idx))
+
+        slices_string = ""
+        for i in range(len(start_idxs)):
+            slices_string += "{}:{},".format(start_idxs[i], end_idxs[i])
+        slices_string = slices_string[:-1]
+
+        summary = f"\n\n\nBaseline result in the slice range [{slices_string}]\n"
+        summary += getNpArrayString(self.npy_baseline[tuple(slices)])
+
+        summary += f"\n\n\nAIE result in the slice range [{slices_string}]\n"
+        summary += getNpArrayString(self.npy_aie[tuple(slices)])
+
+        summary += f"\n\n\nAbsolute difference in the slice range [{slices_string}]\n"
+        summary += getNpArrayString(self.abs_diff[tuple(slices)])
+        return summary
+
+    def getSummaryString(self, max_discrepancies_to_show):
+        summary = self.getBaseString()
+        summary += self.getDiscrepenciesString(max_discrepancies_to_show)
+        summary += self.getSlicesString()
+        return summary
+
+
+def compare(npy_baseline, npy_aie, rtol, atol, max_discrepancies_to_show=50):
+    """
+    Returns a string:
+      - Empty string means the test passed.
+      - Non-empty string means the test failed, string contains
+        a description of the failure.
+    """
+
+    if npy_baseline.shape != npy_aie.shape:
+        return "The two outputs have different shapes: {} and {}".format(
+            npy_baseline.shape, npy_aie.shape
+        )
+
+    are_close = np.allclose(npy_baseline, npy_aie, rtol=rtol, atol=atol)
     if are_close:
-        print("CPU and AIE backend values are all close: PASS")
-        return True
+        return ""
 
-    # The values are not the same. Summarize as best as possible (needs
-    # improvement). Error message might look like:
-    #
-    # Values are not all close. Here is a summary of the differences:
-    # Number of positions where values are different is 3 out of 16384
-    # Maximum difference: 1.0
-    #
-    # Discrepancies:
-    # At index: 0 0
-    # AIE value: 481.0
-    # CPU value: 480.0
-    # At index: 0 1
-    # AIE value: 482.0
-    # CPU value: 481.0
-    # At index: 0 2
-    # AIE value: 455.0
-    # CPU value: 454.0
-
-    diff = np.abs(npy_cpu1 - npy_aie1)
-    max_diff = np.max(diff)
-    diff_positions = np.where(diff > 1e-3)
-    num_diff_positions = len(diff_positions[0])
-    aie_values_at_diff_positions = npy_aie1[diff_positions]
-    cpu_values_at_diff_positions = npy_cpu1[diff_positions]
-    summary_string = "Values are not all close. Here is a summary of the differences:\n"
-    summary_string += (
-        "Number of positions where values are different is {} out of {}\n".format(
-            num_diff_positions, npy_cpu1.size
-        )
+    return Summarizer(npy_baseline, npy_aie, rtol, atol).getSummaryString(
+        max_discrepancies_to_show
     )
-    summary_string += "Maximum difference: {}\n".format(max_diff)
-    summary_string += "Discrepancies: \n"
-    max_discrepancies_to_show = 10
-    for i in range(min(max_discrepancies_to_show, num_diff_positions)):
-        summary_string += "At index: "
-        for j in range(len(diff_positions)):
-            summary_string += "{} ".format(diff_positions[j][i])
-        summary_string += "\nAIE value: {}\n".format(aie_values_at_diff_positions[i])
-        summary_string += "CPU value: {}\n".format(cpu_values_at_diff_positions[i])
-    if num_diff_positions > max_discrepancies_to_show:
-        summary_string += "And {} more discrepancies...".format(
-            num_diff_positions - max_discrepancies_to_show
-        )
-
-    print(summary_string)
-    return False
