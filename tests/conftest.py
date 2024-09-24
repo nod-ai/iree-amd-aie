c39c3f997334dfe0c7f5ce33afde02081c0658a6
Maksim Levental <maksim.levental@gmail.com>
2024-09-24 18:53:06 -0400

    Update ci-linux.yml

---
+++ tests/conftest.py
@@ -0,0 +1,114 @@
+import os
+from contextlib import contextmanager
+
+import numpy as np
+import pytest
+from iree._runtime_libs._runtime import VmModule
+
+from iree.compiler import ir
+from iree.compiler._mlir_libs import get_dialect_registry
+from iree.compiler.api import Session, Output, Source
+from iree.compiler.extras import types as T
+from iree.runtime import get_driver, Config, SystemContext
+
+
+for t in [
+    "i8",
+    "i16",
+    "i32",
+    "i64",
+    "si8",
+    "si16",
+    "si32",
+    "si64",
+    "ui8",
+    "ui16",
+    "ui32",
+    "ui64",
+    "f16",
+    "f32",
+    "f64",
+    "bf16",
+]:
+    tf = getattr(T, t)
+    tf.__name__ = t
+
+
+@pytest.fixture
+def iree_session(request) -> Session:
+    s = Session()
+    s.context.append_dialect_registry(get_dialect_registry())
+    s.context.load_all_available_dialects()
+    target_backend = getattr(request, "target_backend", "amd-aie")
+    pipeline = getattr(request, "pipeline", "air")
+    s.set_flags(
+        f"--iree-hal-target-backends={target_backend}",
+        # TODO(max): normalize iree-amdaie/iree-amd-aie in pass strings
+        f"--iree-amdaie-lower-to-aie-pipeline={pipeline}",
+        f"--iree-amd-aie-peano-install-dir={os.getenv('PEANO_INSTALL_DIR')}",
+        f"--iree-amd-aie-install-dir={os.getenv('IREE_INSTALL_DIR')}",
+    )
+    yield s
+
+
+@pytest.fixture
+def session_module(iree_session, tmpdir) -> ir.Module:
+    iree_session.set_flags(
+        f"--iree-hal-dump-executable-files-to={tmpdir}",
+    )
+    with ir.Location.unknown(iree_session.context):
+        module_op = ir.Module.create()
+        with ir.InsertionPoint(module_op.body):
+            yield iree_session, module_op
+
+
+@contextmanager
+def invokable_module(session, module, device="xrt") -> VmModule:
+    source = Source.wrap_buffer(session, str(module).encode())
+    inv = session.invocation()
+    inv.parse_source(source)
+    inv.execute()
+    compiled_flatbuffer = Output.open_membuffer()
+    inv.output_vm_bytecode(compiled_flatbuffer)
+
+    driver = get_driver(device)
+    config = Config(device=driver.create_default_device())
+    ctx = SystemContext(config=config)
+    vm_module = VmModule.copy_buffer(ctx.instance, compiled_flatbuffer.map_memory())
+    ctx.add_vm_module(vm_module)
+
+    try:
+        yield ctx.modules.module
+    finally:
+        inv.close()
+
+
+_np_dtype_to_mlir_type_ctor = {
+    np.int8: T.i8,
+    np.int16: T.i16,
+    np.int32: T.i32,
+    # windows
+    np.intc: T.i32,
+    np.int64: T.i64,
+    # is technically wrong i guess but numpy by default casts python scalars to this
+    # so to support passing lists of ints we map to index type
+    np.longlong: T.index,
+    np.uintp: T.index,
+    np.float16: T.f16,
+    np.float32: T.f32,
+    np.float64: T.f64,
+}
+
+_mlir_type_ctor_to_np_dtype = lambda: {
+    v: k for k, v in _np_dtype_to_mlir_type_ctor.items()
+}
+
+
+def np_dtype_to_mlir_type(np_dtype):
+    if typ := _np_dtype_to_mlir_type_ctor.get(np_dtype):
+        return typ()
+
+
+def mlir_type_to_np_dtype(mlir_type):
+    _mlir_type_to_np_dtype = {v(): k for k, v in _np_dtype_to_mlir_type_ctor.items()}
+    return _mlir_type_to_np_dtype.get(mlir_type)
---
+++ tests/test_matmul.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pytest
+
+from iree.compiler.dialects import arith, tensor, linalg
+from iree.compiler.dialects.arith import _is_float_type
+from iree.compiler.dialects.func import func
+from iree.compiler.extras import types as T
+from .conftest import invokable_module, mlir_type_to_np_dtype
+
+
+def test_smol_matmul(session_module):
+    session, module = session_module
+
+    @func(T.tensor(32, 16, T.i8()), T.tensor(16, 32, T.i8()))
+    def matmul_i8_i32(lhs, rhs):
+        cst = arith.constant(T.i32(), 0)
+        v0 = tensor.empty([32, 32], T.i32())
+        v1 = linalg.fill(cst, outs=[v0])
+        return linalg.matmul(lhs, rhs, outs=[v1])
+
+    arg0 = np.ones((32, 16), dtype=np.int8)
+    arg1 = np.ones((16, 32), dtype=np.int8)
+    with invokable_module(session, module) as module:
+        results = module[matmul_i8_i32.__name__](arg0, arg1).to_host()
+        assert np.array_equal(results, arg0 @ arg1)
+
+
+def emit_matmul(M, K, N, lhs_rhs_type, acc_type):
+    matmul_name = f"{M}x{K}x{N}x{lhs_rhs_type}x{acc_type}"
+
+    init_value = 0
+    if _is_float_type(acc_type):
+        init_value = 0.0
+
+    @func(T.tensor(M, K, lhs_rhs_type), T.tensor(K, N, lhs_rhs_type), name=matmul_name)
+    def matmul(lhs, rhs):
+        cst = arith.constant(acc_type, init_value)
+        v0 = tensor.empty([M, N], acc_type)
+        v1 = linalg.fill(cst, outs=[v0])
+        return linalg.matmul(lhs, rhs, outs=[v1])
+
+    return matmul_name
+
+
+testdata = [
+    (32, 16, 32, T.i8, T.i32),
+    (32, 32, 32, T.i8, T.i32),
+    (64, 32, 64, T.i8, T.i32),
+    (64, 64, 64, T.i8, T.i32),
+    (128, 64, 128, T.i8, T.i32),
+    (128, 128, 128, T.i8, T.i32),
+    (128, 256, 128, T.i8, T.i32),
+    (32, 16, 32, T.f32, T.f32),
+    (32, 32, 32, T.f32, T.f32),
+    (64, 32, 64, T.f32, T.f32),
+    (64, 64, 64, T.f32, T.f32),
+    (128, 128, 128, T.f32, T.f32),
+    (128, 256, 128, T.f32, T.f32),
+]
+
+
+def ids(datum):
+    if callable(datum):
+        return datum.__name__
+    return datum
+
+
+@pytest.mark.parametrize("M, K, N, lhs_rhs_type, acc_type", testdata, ids=ids)
+def test_matmul(session_module, M, K, N, lhs_rhs_type, acc_type):
+    session, module = session_module
+
+    lhs_rhs_type, acc_type = lhs_rhs_type(), acc_type()
+    matmul_name = emit_matmul(M, K, N, lhs_rhs_type, acc_type)
+
+    lhs_rhs_type = mlir_type_to_np_dtype(lhs_rhs_type)
+    acc_type = mlir_type_to_np_dtype(acc_type)
+    arg0 = np.ones((M, K), dtype=lhs_rhs_type)
+    arg1 = np.ones((K, N), dtype=lhs_rhs_type)
+    with invokable_module(session, module) as module:
+        results = module[matmul_name](arg0, arg1).to_host()
+        assert np.array_equal(results, (arg0.astype(acc_type) @ arg1.astype(acc_type)))
