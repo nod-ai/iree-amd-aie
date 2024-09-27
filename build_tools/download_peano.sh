1a034eadf31d15ea484b1f430c0b33542b0d2d8b
James Newling <james.newling@gmail.com>
2024-09-26 22:01:11 -0700

    bump

--- build_tools/download_peano.sh
+++ build_tools/download_peano.sh
@@ -6,6 +6,6 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-RELEASE=19.0.0.2024082221+90abe71b
+RELEASE=19.0.0.2024092601+562ccea2
 pip download llvm_aie==$RELEASE -f https://github.com/Xilinx/llvm-aie/releases/expanded_assets/nightly
 unzip llvm_aie*whl
