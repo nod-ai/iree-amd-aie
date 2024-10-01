942778850e0268b059051fb0464d69f165383fd5
James Newling <james.newling@gmail.com>
2024-10-01 13:40:51 -0700

    first commit

--- compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/AMDAIEDistributeCoresAndObjectFifos.cpp
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/AMDAIEDistributeCoresAndObjectFifos.cpp
@@ -636,18 +636,20 @@ LogicalResult insertLogicalObjectFifoAccess(ModuleOp moduleOp) {
     DenseMap<Value, AMDAIE::LogicalObjectFifoAccessOp>
         memrefToLogicalObjectFifoAccess;
 
-    WalkResult res = coreOp->walk([&](Operation *op) {
-      bool hasAllocOperand = [op]() {
-        for (Value operand : op->getOperands()) {
-          if (isa_and_present<memref::AllocOp>(operand.getDefiningOp()))
-            return true;
-        }
-        return false;
-      }();
 
-      if (!hasAllocOperand) {
-        return WalkResult::advance();
-      }
+    WalkResult res = coreOp->walk([&](Operation *op) {
+
+    // bool hasAllocOperand = [op]() {
+    //     for (Value operand : op->getOperands()) {
+    //       if (isa_and_present<memref::AllocOp>(operand.getDefiningOp()))
+    //         return true;
+    //     }
+    //     return false;
+    //   }();
+
+    //   if (!hasAllocOperand) {
+    //     return WalkResult::advance();
+    //   }
       // We want to insert amdaie.logicalobjectfifo.access ops right before
       // the first usage. But for vectorized ops this would mean they'd get
       // inserted within the vectorized scf.for ops. We therefore would want
@@ -659,6 +661,11 @@ LogicalResult insertLogicalObjectFifoAccess(ModuleOp moduleOp) {
         opToInsertRewriterPoint = opToInsertRewriterPoint->getParentOp();
       }
       for (auto &&[idx, operand] : llvm::enumerate(op->getOpOperands())) {
+         {
+           Operation *definingOp = operand.get().getDefiningOp();
+           if (!definingOp || !isa<memref::AllocOp>(definingOp)) continue;
+         }
+
         if (memrefToLogicalObjectFifoAccess.contains(operand.get())) {
           op->setOperand(idx, memrefToLogicalObjectFifoAccess[operand.get()]);
         } else if (memrefToLogicalObjectFifo.contains(operand.get())) {
@@ -674,10 +681,12 @@ LogicalResult insertLogicalObjectFifoAccess(ModuleOp moduleOp) {
                        llvm::dyn_cast<MemRefType>(operand.get().getType())) {
           Value memref = operand.get();
           rewriter.setInsertionPoint(coreOp);
+
           auto logicalObjectFifo =
               rewriter.create<AMDAIE::LogicalObjectFifoFromMemrefOp>(
                   rewriter.getUnknownLoc(), LogicalObjectFifoType::get(type),
                   memref);
+
           rewriter.setInsertionPoint(opToInsertRewriterPoint);
 
           AMDAIE::LogicalObjectFifoAccessOp accessOp;
