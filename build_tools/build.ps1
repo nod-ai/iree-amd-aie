1fd9113b859adad5de2c640717fcd158203d87b7
dezhliao <dezhi.liao@amd.com>
2024-10-18 16:04:55 -0700

    Remove unrelated build scripts

--- build_tools/build.ps1
+++
@@ -1,8 +0,0 @@
-$this_dir = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
-$repo_root = Resolve-Path -Path "$this_dir/.."
-$iree_dir = Resolve-Path -Path "$repo_root/third_party/iree"
-$build_dir = "$repo_root/iree-build"
-
-echo "Building all"
-echo "------------"
-& cmake --build $build_dir --verbose
