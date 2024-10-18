1a3efc4682210648052538a0e53e93ffdc1e22eb
makslevental <maksim.levental@gmail.com>
2024-10-17 20:34:58 -0400

    incorporate comments

--- runtime/src/iree-amd-aie/driver/xrt-lite/shim/linux/kmq/shim_debug.cpp
+++ runtime/src/iree-amd-aie/driver/xrt-lite/shim/linux/kmq/shim_debug.cpp
@@ -1,6 +1,8 @@
+// Copyright 2024 The IREE Authors
 //
-// Created by mlevental on 10/3/24.
-//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "shim_debug.h"
 
