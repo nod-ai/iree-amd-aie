700f86985d51e7531f990297a938036f9dc6f713
James Newling <james.newling@gmail.com>
2024-11-20 17:08:25 -0800

    better printing in CI

--- build_tools/ci/cpu_comparison/performance_summarizer.py
+++ build_tools/ci/cpu_comparison/performance_summarizer.py
@@ -6,15 +6,22 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 2:
-        print("Usage: python3 performance_summarizer.py <path_to_log_file>")
+        print(
+            "Usage: python3 performance_summarizer.py <path_to_log_file>. This will strip out the performance numbers from the log file and print a summary."
+        )
         sys.exit(1)
     path = sys.argv[1]
     with open(path, "r") as f:
         lines = f.readlines()
     print("============================")
+    first_print = True
     for line in lines:
         if "Run #1" in line:
-            print(line.split()[-1])
+            if not first_print:
+                print("\n" + line.split()[-1])
+            else:
+                print(line.split()[-1])
+            first_print = False
         if "IREE_AMDAIE" in line:
-            print(line)
+            print(line.strip())
     print("============================")
