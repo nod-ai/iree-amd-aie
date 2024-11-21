1e1f87e955f2ad1bea0b6a1eef2dcc850d7a240a
Jorn Tuyls <jorn.tuyls@gmail.com>
2024-11-21 07:32:46 -0800

    Add control code forall to for pass

--- compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/Passes.cpp
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/Passes.cpp
@@ -622,6 +622,12 @@ void addAMDAIEObjectFifoLoweringPasses(
   passManager.addPass(createCSEPass());
   passManager.addPass(createCanonicalizerPass());
 
+  // Convert control code `scf.forall` ops to `scf.for` ops right before the DMA
+  // composition optimization pass to enable more loop subsumption optimization
+  // opportunities.
+  passManager.addPass(createAMDAIEControlCodeForallToForPass());
+  passManager.addPass(createCSEPass());
+  passManager.addPass(createCanonicalizerPass());
   passManager.addPass(createAMDAIEDmaCompositionPass());
   passManager.addPass(createCSEPass());
   passManager.addPass(createCanonicalizerPass());
