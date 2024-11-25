98333d81bcc7daa6366fb1414800497035125f46
Jorn Tuyls <jorn.tuyls@gmail.com>
2024-11-25 03:07:01 -0800

    [SplitLogicalObjFifos] Generalize splitting analysis and enable pass in pipeline

--- compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/AMDAIESplitLogicalObjFifos.cpp
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/AMDAIESplitLogicalObjFifos.cpp
@@ -13,6 +13,150 @@
 
 namespace mlir::iree_compiler::AMDAIE {
 
+/// Utility to compute the product of the sizes after each dimension:
+/// productSizes[idx] will contain the product of all sizes in [idx + 1,
+/// sizes.size() - 1].
+SmallVector<int64_t> getProductSizesAfterIndex(ArrayRef<int64_t> sizes) {
+  SmallVector<int64_t> productSizes;
+  productSizes.reserve(sizes.size());
+  int64_t productSize{1};
+  for (auto size : llvm::reverse(sizes)) {
+    productSize *= size;
+    productSizes.push_back(productSize);
+  }
+  std::reverse(productSizes.begin(), productSizes.end());
+  return productSizes;
+}
+
+/// Utility struct to represent DMA split information.
+struct DmaSplitInfo {
+  size_t sourceSplitDim;
+  size_t targetSplitDim;
+};
+
+using DmaObjFifoPairT =
+    std::pair<AMDAIE::DmaCpyNdOp, AMDAIE::LogicalObjectFifoFromMemrefOp>;
+
+/// Find the logical objectFifo and DMA source/target splitting dimensions for
+/// each DMA and objectFifo pair.
+///
+/// Each pair is handled in the following way:
+/// First, compute the objectFifo splitting dimension as the last non-unit shape
+/// dimension. Afterwards, depending on which logical objectFifo is being
+/// split on, find the outermost dimension in either the source or
+/// target access pattern that has:
+/// - stride == sizeAfterSplit
+/// - size != 1
+/// This is the splitting dimension to be used on the respective side of the DMA
+/// operation. Then, calculate the product size of that side of the DMA
+/// operation after the splitting dimension and use it to calculate the
+/// splitting dimension on the other side as the first dimension from the back
+/// that has product size larger than the other side's product size after
+/// splitting because that's the number of elements that should be
+/// produced/consumed on the respective sides before splitting.
+LogicalResult collectSplittingDims(
+    const SmallVector<DmaObjFifoPairT> &dmaObjFifoPairs,
+    DenseMap<AMDAIE::DmaCpyNdOp, DmaSplitInfo> &dmaSplitInfoMap,
+    DenseMap<AMDAIE::LogicalObjectFifoFromMemrefOp, size_t>
+        &objFifoSplitDimMap) {
+  for (auto [dmaOp, objFifo] : dmaObjFifoPairs) {
+    LLVM_DEBUG(llvm::dbgs() << "dmaOp: " << dmaOp << "\n");
+    LLVM_DEBUG(llvm::dbgs() << "objFifo: " << objFifo << "\n");
+    ArrayRef<int64_t> memrefShape = objFifo.getMemrefType().getShape();
+    if (llvm::any_of(memrefShape, [](int64_t size) { return size < 0; })) {
+      return objFifo.emitOpError()
+             << "can't find a valid split dimension for dynamic sizes memref";
+    }
+    size_t objFifoSplitDim{0};
+    for (int i = 0; i < memrefShape.size(); i++) {
+      if (memrefShape[i] != 1) {
+        objFifoSplitDim = i;
+        break;
+      }
+    }
+    int64_t sizeAfterSplit =
+        std::accumulate(memrefShape.begin() + objFifoSplitDim + 1,
+                        memrefShape.end(), 1, std::multiplies<>());
+
+    size_t sourceSplitDim{0};
+    size_t targetSplitDim{0};
+    if (dmaOp.getTargetObjectFifo() == objFifo) {
+      std::optional<SmallVector<int64_t>> targetSizes =
+          getConstantIntValues(dmaOp.getTargetMixedSizes());
+      std::optional<SmallVector<int64_t>> targetStrides =
+          getConstantIntValues(dmaOp.getTargetMixedStrides());
+      std::optional<SmallVector<int64_t>> sourceSizes =
+          getConstantIntValues(dmaOp.getSourceMixedSizes());
+      if (!targetSizes.has_value() || !targetStrides.has_value() ||
+          !sourceSizes.has_value()) {
+        return dmaOp.emitOpError() << "has unsupported dynamic target strides "
+                                      "or sizes or source sizes";
+      }
+      for (auto iter : llvm::enumerate(
+               llvm::zip(targetSizes.value(), targetStrides.value()))) {
+        int64_t size = std::get<0>(iter.value());
+        int64_t stride = std::get<1>(iter.value());
+        if (stride == sizeAfterSplit && size != 1) {
+          targetSplitDim = iter.index();
+          break;
+        }
+      }
+      int64_t targetSizeAfterSplit =
+          std::accumulate(targetSizes.value().begin() + targetSplitDim + 1,
+                          targetSizes.value().end(), 1, std::multiplies<>());
+      SmallVector<int64_t> sourceProductSizes =
+          getProductSizesAfterIndex(sourceSizes.value());
+      for (int idx = sourceProductSizes.size() - 1; idx > 0; idx--) {
+        if (sourceProductSizes[idx] > targetSizeAfterSplit) {
+          sourceSplitDim = idx;
+          break;
+        }
+      }
+    } else if (dmaOp.getSourceObjectFifo() == objFifo) {
+      // Find outermost dimension in the access pattern that has stride ==
+      // sizeAfterSplit and size != 1.
+      std::optional<SmallVector<int64_t>> sourceSizes =
+          getConstantIntValues(dmaOp.getSourceMixedSizes());
+      std::optional<SmallVector<int64_t>> sourceStrides =
+          getConstantIntValues(dmaOp.getSourceMixedStrides());
+      std::optional<SmallVector<int64_t>> targetSizes =
+          getConstantIntValues(dmaOp.getTargetMixedSizes());
+      if (!sourceSizes.has_value() || !sourceStrides.has_value() ||
+          !targetSizes.has_value()) {
+        return dmaOp.emitOpError() << "has unsupported dynamic source strides "
+                                      "or sizes or target sizes";
+      }
+      for (auto iter : llvm::enumerate(
+               llvm::zip(sourceSizes.value(), sourceStrides.value()))) {
+        int64_t size = std::get<0>(iter.value());
+        int64_t stride = std::get<1>(iter.value());
+        if (stride == sizeAfterSplit && size != 1) {
+          sourceSplitDim = iter.index();
+          break;
+        }
+      }
+      int64_t sourceRemainderSize =
+          std::accumulate(sourceSizes.value().begin() + sourceSplitDim + 1,
+                          sourceSizes.value().end(), 1, std::multiplies<>());
+      SmallVector<int64_t> targetProductSizes =
+          getProductSizesAfterIndex(targetSizes.value());
+      for (int idx = targetProductSizes.size() - 1; idx > 0; idx--) {
+        if (targetProductSizes[idx] > sourceRemainderSize) {
+          targetSplitDim = idx;
+          break;
+        }
+      }
+    }
+    LLVM_DEBUG(llvm::dbgs() << "sourceSplitDim: " << sourceSplitDim << "\n");
+    LLVM_DEBUG(llvm::dbgs() << "targetSplitDim: " << targetSplitDim << "\n");
+    LLVM_DEBUG(llvm::dbgs() << "objFifoSplitDim: " << objFifoSplitDim << "\n");
+    DmaSplitInfo dmaSplitInfo = {sourceSplitDim, targetSplitDim};
+    dmaSplitInfoMap[dmaOp] = std::move(dmaSplitInfo);
+    objFifoSplitDimMap[objFifo] = objFifoSplitDim;
+  }
+  return success();
+}
+
 namespace {
 
 /// Utility struct to represent DMA split information.
