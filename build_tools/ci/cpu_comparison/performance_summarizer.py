2940ed530a31f7a0ad06dda95e391323386b4009
Yu-Zhewen <zhewenyu@amd.com>
2024-11-26 00:25:06 +0000

    update performance_summarizer script

--- build_tools/ci/cpu_comparison/performance_summarizer.py
+++ build_tools/ci/cpu_comparison/performance_summarizer.py
@@ -13,15 +13,28 @@ if __name__ == "__main__":
     path = sys.argv[1]
     with open(path, "r") as f:
         lines = f.readlines()
+
     print("============================")
-    first_print = True
+    test_name = "Unknown"
+    kernel_times = []
+
     for line in lines:
-        if "Run #1" in line:
-            if not first_print:
-                print("\n" + line.split()[-1])
-            else:
-                print(line.split()[-1])
-            first_print = False
-        if "IREE_AMDAIE" in line:
+        if "Run #1 of" in line:
+            if kernel_times:
+                print(
+                    f"Average kernel time for {test_name}: {sum(kernel_times) / len(kernel_times):.4f} ms"
+                )
+            test_name = line.split("/")[-1].strip()
+            kernel_times = []
+            print(f"\n{test_name}")
+        elif "IREE_AMDAIE" in line:
             print(line.strip())
+            kernel_times.append(
+                float(line.split("Kernel time:")[1].split("[ms]")[0].strip())
+            )
+
+    if kernel_times:
+        print(
+            f"Average kernel time for {test_name}: {sum(kernel_times) / len(kernel_times):.4f} ms"
+        )
     print("============================")
