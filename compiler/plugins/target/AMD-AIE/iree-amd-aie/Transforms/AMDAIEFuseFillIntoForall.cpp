e1ecc84c2ebdb86f22c34de22a87acdbea0ac7a9
James Newling <james.newling@gmail.com>
2024-12-10 12:36:15 -0800

    address review comments

--- compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/AMDAIEFuseFillIntoForall.cpp
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/AMDAIEFuseFillIntoForall.cpp
@@ -30,30 +30,53 @@ class AMDAIEFuseFillIntoForallPass
 
 void AMDAIEFuseFillIntoForallPass::runOnOperation() {
   MLIRContext *context = &getContext();
-  mlir::FunctionOpInterface funcOp = getOperation();
   IRRewriter rewriter(context);
 
   // Find a unique FillOp with a single output, or return.
   SmallVector<linalg::FillOp> fillOps;
-  funcOp->walk([&](linalg::FillOp fillOp) { fillOps.push_back(fillOp); });
-  if (fillOps.size() != 1) return;
+  getOperation()->walk([&](linalg::FillOp fillOp) { fillOps.push_back(fillOp); });
+  if (fillOps.size() != 1) {
+    LLVM_DEBUG(llvm::dbgs() << "Expected exactly 1 fill op, but found "
+                            << fillOps.size() << ".\n");
+    return;
+  }
+
   linalg::FillOp fillOp = fillOps[0];
-  if (fillOp.getResults().size() != 1) return;
+  if (fillOp.getResults().size() != 1) {
+    LLVM_DEBUG(llvm::dbgs() << "Expected fill op to have exactly 1 result, but "
+                            << "found " << fillOp.getResults().size() << ".\n");
+
+    return;
+  };
 
   // Confirm that there is a unique user that is a forall, and match
   // the block argument that is used by the fill op, or return.
   ResultRange::use_range fillUses = fillOp->getUses();
-  if (std::distance(fillUses.begin(), fillUses.end()) != 1) return;
+  if (std::distance(fillUses.begin(), fillUses.end()) != 1) {
+    LLVM_DEBUG(llvm::dbgs()
+               << "Expected exactly 1 use of fill op, but found "
+               << std::distance(fillUses.begin(), fillUses.end()) << ".\n");
+    return;
+  }
   OpOperand &fillUse = *fillUses.begin();
   auto forallOp = dyn_cast<scf::ForallOp>(fillUse.getOwner());
-  if (!forallOp) return;
+  if (!forallOp) {
+    LLVM_DEBUG(llvm::dbgs() << "Expected fill op to be used by a forall op, "
+                            << "but the only user is "
+                            << fillUse.getOwner()->getName() << ".\n");
+    return;
+  }
   BlockArgument bbArg = forallOp.getTiedBlockArgument(&fillUse);
 
   // Find 0 or 1 ExtractSliceOps that use the fill result, or return.
   tensor::ExtractSliceOp extractSliceOp;
   for (Operation *user : bbArg.getUsers()) {
     if (auto nxt = dyn_cast<tensor::ExtractSliceOp>(user)) {
-      if (extractSliceOp) return;
+      if (extractSliceOp) {
+        LLVM_DEBUG(llvm::dbgs()
+                   << "Expected at most 1 extract_slice op, but found 2+.\n");
+        return;
+      }
       extractSliceOp = nxt;
     }
   }
@@ -67,33 +90,30 @@ void AMDAIEFuseFillIntoForallPass::runOnOperation() {
         scf::tileAndFuseProducerOfSlice(rewriter, extractSliceOp,
                                         MutableArrayRef(&loops, 1));
     if (!fusedProducer) {
-      funcOp->emitOpError("Failed to fuse fill op into forall loop.");
+      fillOp->emitOpError("could not be fused into forall");
       return signalPassFailure();
     }
-    return;
-  }
-
-  // In the case where there are no extract_slice ops, we manually create the
-  // fill at the beginning of the forall body.
-  assert(!extractSliceOp);
-  rewriter.setInsertionPointToStart(forallOp.getBody());
-  Value scalar = fillOp.value();
-  Location loc = fillOp.getLoc();
-  auto fusedFill = rewriter.create<linalg::FillOp>(loc, scalar, bbArg);
-  rewriter.replaceUsesWithIf(
-      bbArg, fusedFill.getResult(0), [&](OpOperand &operand) {
-        Operation *owner = operand.getOwner();
-        if (owner == fusedFill) {
-          return false;
-        } else if (isa<tensor::ParallelInsertSliceOp>(owner)) {
-          return false;
-        } else {
+  } else {
+    // In the case where there are no extract_slice ops, we manually create the
+    // fill at the beginning of the forall body. This situation might arise
+    // if the extract_slice has been folded, for example if the forall is
+    // over a grid if size 1.
+    rewriter.setInsertionPointToStart(forallOp.getBody());
+    Value scalar = fillOp.value();
+    Location loc = fillOp.getLoc();
+    auto fusedFill = rewriter.create<linalg::FillOp>(loc, scalar, bbArg);
+    rewriter.replaceUsesWithIf(
+        bbArg, fusedFill.getResult(0), [&](OpOperand &operand) {
+          Operation *owner = operand.getOwner();
+          if (owner == fusedFill || isa<tensor::ParallelInsertSliceOp>(owner)) {
+            return false;
+          }
           return true;
-        }
-      });
+        });
 
-  // Do not use the result of the old fill.
-  rewriter.replaceAllUsesWith(fillOp.getResults()[0], fillOp.getOutputs()[0]);
+    // Do not use the result of the old fill.
+    rewriter.replaceAllUsesWith(fillOp.getResults()[0], fillOp.getOutputs()[0]);
+  }
 }
 
 }  // namespace
