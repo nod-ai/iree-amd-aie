ec53786f07fbdef48a9a4d3bf41e393ed675c4e4
Yu-Zhewen <zhewenyu@amd.com>
2025-01-08 14:29:02 +0000

    init commit

--- runtime/src/iree-amd-aie/aie_runtime/Utils/test/ChannelGeneratorTest.cpp
+++ runtime/src/iree-amd-aie/aie_runtime/Utils/test/ChannelGeneratorTest.cpp
@@ -15,26 +15,51 @@ using namespace mlir::iree_compiler::AMDAIE;
 
 TEST(ChannelGeneratorTest, GetAssign) {
   ChannelGenerator generator(2, 2);
-  EXPECT_EQ(generator.getAndAssignProducerDMAChannel().value(), 0);
-  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel().value(), 0);
-  EXPECT_EQ(generator.getAndAssignProducerDMAChannel().value(), 1);
-  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel().value(), 1);
-  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(), std::nullopt);
-  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(), std::nullopt);
+  bool isPacketFlow = false;
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow).value(), 0);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow).value(), 0);
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow).value(), 1);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow).value(), 1);
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow),
+            std::nullopt);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow),
+            std::nullopt);
 }
 
 TEST(ChannelGeneratorTest, Occupied) {
+  ChannelGenerator generator(4, 4);
+  bool isPacketFlow = false;
+  generator.assignProducerDMAChannel(0);
+  generator.assignConsumerDMAChannel(0);
+  generator.assignProducerDMAChannel(2);
+  generator.assignConsumerDMAChannel(2);
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow).value(), 1);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow).value(), 1);
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow).value(), 3);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow).value(), 3);
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow),
+            std::nullopt);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow),
+            std::nullopt);
+}
+
+TEST(ChannelGeneratorTest, PacketFlow) {
   ChannelGenerator generator(4, 4);
   generator.assignProducerDMAChannel(0);
   generator.assignConsumerDMAChannel(0);
   generator.assignProducerDMAChannel(2);
   generator.assignConsumerDMAChannel(2);
-  EXPECT_EQ(generator.getAndAssignProducerDMAChannel().value(), 1);
-  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel().value(), 1);
-  EXPECT_EQ(generator.getAndAssignProducerDMAChannel().value(), 3);
-  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel().value(), 3);
-  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(), std::nullopt);
-  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(), std::nullopt);
+  bool isPacketFlow = true;
+  // Packet flow should not occupy the channel exclusively, and the available
+  // channel appears in a round-robin fashion.
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow).value(), 1);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow).value(), 1);
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow).value(), 3);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow).value(), 3);
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow).value(), 1);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow).value(), 1);
+  EXPECT_EQ(generator.getAndAssignProducerDMAChannel(isPacketFlow).value(), 3);
+  EXPECT_EQ(generator.getAndAssignConsumerDMAChannel(isPacketFlow).value(), 3);
 }
 
 }  // namespace
