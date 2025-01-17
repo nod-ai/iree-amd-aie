c3ffc4cc6726da01a002f9a3e3c3fc29642bebc5
saienduri <saimanas.enduri@amd.com>
2025-01-16 19:45:28 -0800

    skip license

--- build_tools/ci/install_cmake.sh
+++ build_tools/ci/install_cmake.sh
@@ -13,7 +13,7 @@ cd ~/temp
 
 wget https://cmake.org/files/v$version/cmake-$version.$build-$os-x86_64.sh
 sudo mkdir -p /opt/cmake
-sudo sh cmake-$version.$build-$os-x86_64.sh --prefix=/opt/cmake
+sudo sh cmake-$version.$build-$os-x86_64.sh --prefix=/opt/cmake --skip-license
 sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 
 rm -rf ~/temp
