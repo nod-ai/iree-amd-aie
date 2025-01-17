1460d6b300b5bca6442dca9f0ebeda7430998637
saienduri <saimanas.enduri@amd.com>
2025-01-16 19:35:19 -0800

    cmake update

--- build_tools/ci/install_cmake.sh
+++ build_tools/ci/install_cmake.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install -y bc wget
+
 version=3.28
 build=1
 limit=3.20
@@ -11,5 +14,6 @@ cd ~/temp
 wget https://cmake.org/files/v$version/cmake-$version.$build-$os-x86_64.sh
 sudo mkdir -p /opt/cmake
 sudo sh cmake-$version.$build-$os-x86_64.sh --prefix=/opt/cmake
+sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 
 rm -rf ~/temp
