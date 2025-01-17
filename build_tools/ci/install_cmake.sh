2422b209fa47a0f85f7f6b6c8f66045529f016d9
saienduri <saimanas.enduri@amd.com>
2025-01-16 22:36:38 -0800

    ctest path

--- build_tools/ci/install_cmake.sh
+++ build_tools/ci/install_cmake.sh
@@ -15,5 +15,6 @@ wget https://cmake.org/files/v$version/cmake-$version.$build-$os-x86_64.sh
 sudo mkdir -p /opt/cmake
 sudo sh cmake-$version.$build-$os-x86_64.sh --prefix=/opt/cmake --skip-license
 sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+sudo ln -s /opt/cmake/bin/ctest /usr/local/bin/ctest
 
 rm -rf ~/temp
