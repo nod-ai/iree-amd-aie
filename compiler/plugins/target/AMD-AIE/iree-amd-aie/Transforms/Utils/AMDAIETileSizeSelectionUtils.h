90ab8d6b180c9ec7ed506b93e8fc8d70a6afddba
Vivian Zhang <vivian.zhang@amd.com>
2025-04-08 21:35:48 -0700

    [LoweringStrategy] Refactor the logic of L1 tile size selection (#1214)
    Refactor the logic to make the most use of L1 memory. The
    result of the change is using a larger k tile size, however, this causes
    many failure of tests. Also increasing k tile size can cause performance
    regression and we haven't found the cause, ref issue
    https://github.com/nod-ai/iree-amd-aie/issues/985.
    
    Update:
    1. Tested vectorization path with k tile size as 128. 
    - For bf16->f32 tests, all could compile with outline, however the
    performance degrades about 20% compared to k = 32.
    - For i8 to i32, all could compile, but some tests generates
    intermittent or consistent numeric error.
    - So for now using max k tile size with ukernel path, while keeping
    smaller size for vectorization path.
    
    2. For ukernel with larger tile sizes, I see around 15~30% performance
    improvement on Phoenix and around 10%~23% improvement on Strix. For
    example,
    
    Before
    
    ```
    matmul_512_4096_512_bf16_f32_O3_npu1_4col_outline_ukernel_chess_benchmark
    --------------------------------------------------------------------------------------------------
    Benchmark                                        Time             CPU   Iterations UserCounters...
    --------------------------------------------------------------------------------------------------
    BM_matmul/process_time/real_time_mean         2694 us         52.3 us            5 items_per_second=371.513/s
    BM_matmul/process_time/real_time_median       2696 us         50.9 us            5 items_per_second=370.985/s
    BM_matmul/process_time/real_time_stddev       92.9 us         3.26 us            5 items_per_second=12.9349/s
    --------------------------------------------------------------------------------------------------
    
    ```
    After
    
    ```
    matmul_512_4096_512_bf16_f32_O3_npu1_4col_outline_ukernel_chess_benchmark
    --------------------------------------------------------------------------------------------------
    Benchmark                                        Time             CPU   Iterations UserCounters...
    --------------------------------------------------------------------------------------------------
    BM_matmul/process_time/real_time_mean         1877 us         55.0 us            5 items_per_second=532.731/s
    BM_matmul/process_time/real_time_median       1876 us         56.1 us            5 items_per_second=532.923/s
    BM_matmul/process_time/real_time_stddev       11.9 us         2.46 us            5 items_per_second=3.39164/s
    --------------------------------------------------------------------------------------------------
    --------------------------------------------------------------------------------------------------
    ```

---
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/Utils/AMDAIETileSizeSelectionUtils.h
@@ -0,0 +1,33 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_AMD_AIE_TRANSFORMS_AMDAIETILESIZESELECTIONUTILS_H_
+#define IREE_AMD_AIE_TRANSFORMS_AMDAIETILESIZESELECTIONUTILS_H_
+
+#include <stdint.h>
+
+namespace mlir::iree_compiler::AMDAIE {
+
+struct TileParams {
+  int64_t memoryLimit;
+  uint32_t numBytesA, numBytesB, numBytesC, numBytesAcc;
+  uint32_t bufferDepthA, bufferDepthB, bufferDepthC, bufferDepthAcc;
+  uint32_t inputM, inputN, inputK;
+  uint32_t vectorM, vectorN, vectorK;
+};
+
+struct TileSize {
+  uint32_t M, N, K;
+  bool operator==(const TileSize& tileSize) const {
+    return M == tileSize.M && N == tileSize.N && K == tileSize.K;
+  }
+};
+
+TileSize selectL1TileSizes(const TileParams& params);
+
+}  // namespace mlir::iree_compiler::AMDAIE
+
+#endif
