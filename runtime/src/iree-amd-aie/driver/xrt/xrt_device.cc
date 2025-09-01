da4e7380643fb708184c1c1c67f95139849e444c
Abhishek Varma <abhvarma@amd.com>
2025-09-01 11:18:11 +0000

    Bump IREE to iree-org/iree@c37c680ae6e71
    Signed-off-by: Abhishek Varma <abhvarma@amd.com>

--- runtime/src/iree-amd-aie/driver/xrt/xrt_device.cc
+++ runtime/src/iree-amd-aie/driver/xrt/xrt_device.cc
@@ -230,8 +230,9 @@ static iree_status_t iree_hal_xrt_device_import_file(
 }
 
 static iree_status_t iree_hal_xrt_device_create_semaphore(
-    iree_hal_device_t* base_device, uint64_t initial_value,
-    iree_hal_semaphore_flags_t flags, iree_hal_semaphore_t** out_semaphore) {
+    iree_hal_device_t* base_device, iree_hal_queue_affinity_t queue_affinity,
+    uint64_t initial_value, iree_hal_semaphore_flags_t flags,
+    iree_hal_semaphore_t** out_semaphore) {
   iree_hal_xrt_device_t* device = iree_hal_xrt_device_cast(base_device);
   return iree_hal_xrt_semaphore_create(device->host_allocator, initial_value,
                                        out_semaphore);
@@ -251,8 +252,9 @@ static iree_status_t iree_hal_xrt_device_queue_alloca(
     iree_device_size_t allocation_size, iree_hal_execute_flags_t flags,
     iree_hal_buffer_t** IREE_RESTRICT out_buffer) {
   // TODO: queue-ordered allocations.
-  IREE_RETURN_IF_ERROR(iree_hal_semaphore_list_wait(wait_semaphore_list,
-                                                    iree_infinite_timeout()));
+  IREE_RETURN_IF_ERROR(
+      iree_hal_semaphore_list_wait(wait_semaphore_list, iree_infinite_timeout(),
+                                   IREE_HAL_WAIT_FLAG_DEFAULT));
   IREE_RETURN_IF_ERROR(
       iree_hal_allocator_allocate_buffer(iree_hal_device_allocator(base_device),
                                          params, allocation_size, out_buffer));
@@ -265,8 +267,9 @@ static iree_status_t iree_hal_xrt_device_queue_dealloca(
     const iree_hal_semaphore_list_t wait_semaphore_list,
     const iree_hal_semaphore_list_t signal_semaphore_list,
     iree_hal_buffer_t* buffer, iree_hal_alloca_flags_t flags) {
-  IREE_RETURN_IF_ERROR(iree_hal_semaphore_list_wait(wait_semaphore_list,
-                                                    iree_infinite_timeout()));
+  IREE_RETURN_IF_ERROR(
+      iree_hal_semaphore_list_wait(wait_semaphore_list, iree_infinite_timeout(),
+                                   IREE_HAL_WAIT_FLAG_DEFAULT));
   iree_status_t status = iree_hal_semaphore_list_signal(signal_semaphore_list);
   return status;
 }
@@ -352,7 +355,8 @@ static iree_status_t iree_hal_xrt_device_queue_flush(
 
 static iree_status_t iree_hal_xrt_device_wait_semaphores(
     iree_hal_device_t* base_device, iree_hal_wait_mode_t wait_mode,
-    const iree_hal_semaphore_list_t semaphore_list, iree_timeout_t timeout) {
+    const iree_hal_semaphore_list_t semaphore_list, iree_timeout_t timeout,
+    iree_hal_wait_flags_t flags) {
   return iree_make_status(IREE_STATUS_UNIMPLEMENTED,
                           "Unimplemented semaphore wait");
 }
